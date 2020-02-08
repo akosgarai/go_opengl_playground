500e6649af419b5bdec2ecc0f8e469f17247d008
akosgarai <szlem01@gmail.com>
2020-02-08 16:35:24 +0100

    Refactor - move shader to package

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## Useful links:
 
 - https://kylewbanks.com/blog/tutorial-opengl-with-golang-part-1-hello-opengl
+- https://kylewbanks.com/blog/tutorial-opengl-with-golang-part-2-drawing-the-game-board
 - https://medium.com/@drgomesp/opengl-and-golang-getting-started-abcd3d96f3db
--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"runtime"
 
-	"github.com/go-gl/gl/v4.6-core/gl"
+	"github.com/akosgarai/opengl_playground/pkg/shader"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
 )
 
@@ -13,6 +15,33 @@ const (
 	windowHeight = 600
 	windowTitle  = "Hello world"
 )
+
+var (
+	triangle = []float32{
+		0, 0.5, 0, // top
+		-0.5, -0.5, 0, // left
+		0.5, -0.5, 0, // right
+	}
+)
+
+// newVertexArrayObject initializes and returns a vertex array from the points provided.
+func newVertexArrayObject(points []float32) uint32 {
+	var vertexBufferObject uint32
+	gl.GenBuffers(1, &vertexBufferObject)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
+	// a 32-bit float has 4 bytes, so we are saying the size of the buffer,
+	// in bytes, is 4 times the number of points
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+
+	var vertexArrayObject uint32
+	gl.GenVertexArrays(1, &vertexArrayObject)
+	gl.BindVertexArray(vertexArrayObject)
+	gl.EnableVertexAttribArray(0)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)
+
+	return vertexArrayObject
+}
 
 func initGlfw() *glfw.Window {
 	if err := glfw.Init(); err != nil {
@@ -36,28 +65,47 @@ func initGlfw() *glfw.Window {
 	return window
 }
 
-func initOpenGL() {
+func initOpenGL() uint32 {
 	if err := gl.Init(); err != nil {
 		panic(err)
 	}
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
-	gl.ClearColor(0, 0.5, 1.0, 1.0)
+	vertexShader, err := shader.CompileShader(shader.VertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := shader.CompileShader(shader.FragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	program := gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+	return program
 }
 
-func draw(window *glfw.Window) {
+func draw(vertexArrayObject uint32, window *glfw.Window, program uint32) {
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+	gl.UseProgram(program)
+	gl.BindVertexArray(vertexArrayObject)
+	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/3))
 	glfw.PollEvents()
 	window.SwapBuffers()
 }
+
 func main() {
 	runtime.LockOSThread()
 
 	window := initGlfw()
-	initOpenGL()
+	defer glfw.Terminate()
+	program := initOpenGL()
 
+	vertexArrayObject := newVertexArrayObject(triangle)
 	for !window.ShouldClose() {
-		draw(window)
+		draw(vertexArrayObject, window, program)
 	}
 }
