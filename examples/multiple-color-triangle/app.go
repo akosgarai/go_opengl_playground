bb592cf5427684c0c02823aecba1b1c21bde6012
akosgarai <szlem01@gmail.com>
2020-02-11 13:09:45 +0100

    Example 5 - multiple color triangle

---
+++ examples/multiple-color-triangle/app.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/akosgarai/opengl_playground/pkg/primitives"
+	"github.com/akosgarai/opengl_playground/pkg/shader"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+const (
+	windowWidth  = 800
+	windowHeight = 600
+	windowTitle  = "Example - static triangle with multiple color"
+)
+
+var (
+	triangle = primitives.NewTriangle(
+		primitives.Vector{-0.75, 0.75, 0}, // top
+		primitives.Vector{-0.75, 0.25, 0}, // left
+		primitives.Vector{-0.25, 0.25, 0}, // right
+	)
+)
+
+func initGlfw() *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(fmt.Errorf("could not initialize glfw: %v", err))
+	}
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.Resizable, glfw.True)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, windowTitle, nil, nil)
+
+	if err != nil {
+		panic(fmt.Errorf("could not create opengl renderer: %v", err))
+	}
+
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func initOpenGL() uint32 {
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+	version := gl.GoStr(gl.GetString(gl.VERSION))
+	fmt.Println("OpenGL version", version)
+
+	vertexShader, err := shader.CompileShader(shader.VertexShaderCookBookSource, gl.VERTEX_SHADER)
+	if err != nil {
+		panic(err)
+	}
+	fragmentShader, err := shader.CompileShader(shader.FragmentShaderCookBookSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		panic(err)
+	}
+
+	program := gl.CreateProgram()
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.LinkProgram(program)
+	return program
+}
+
+func main() {
+	runtime.LockOSThread()
+
+	window := initGlfw()
+	defer glfw.Terminate()
+	program := initOpenGL()
+
+	// Configure global settings
+	gl.UseProgram(program)
+	uniform := [16]float32{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
+	// mvp - modelview - projection matrix
+	mvp := gl.GetUniformLocation(program, gl.Str("MVP\x00"))
+	gl.UniformMatrix4fv(mvp, 1, false, &uniform[0])
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	// red
+	triangle.A.SetColor(primitives.Vector{1, 0, 0})
+	// green
+	triangle.B.SetColor(primitives.Vector{0, 1, 0})
+	// blue
+	triangle.C.SetColor(primitives.Vector{0, 0, 1})
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+		gl.UseProgram(program)
+		triangle.Draw()
+		glfw.PollEvents()
+		window.SwapBuffers()
+	}
+}
