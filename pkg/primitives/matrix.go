df932e6d8b86a6b3d0b13a4f7748ca2fd83fd571
akosgarai <szlem01@gmail.com>
2020-02-13 12:51:18 +0100

    Add more functions for creating matrix
    projection matrix
    translation matrix
    scale matrix

--- pkg/primitives/matrix.go
+++ pkg/primitives/matrix.go
@@ -1,5 +1,9 @@
 package primitives
 
+import (
+	"math"
+)
+
 type Matrix4x4 struct {
 	Points [16]float32
 }
@@ -23,6 +27,39 @@ func UnitMatrix4x4() *Matrix4x4 {
 			0.0, 0.0, 0.0, 1.0,
 		},
 	}
+}
+func ScaleMatrix4x4(scaleX, scaleY, scaleZ float32) *Matrix4x4 {
+	return &Matrix4x4{
+		[16]float32{
+			scaleX, 0.0, 0.0, 0.0,
+			0.0, scaleY, 0.0, 0.0,
+			0.0, 0.0, scaleZ, 0.0,
+			0.0, 0.0, 0.0, 1.0,
+		},
+	}
+}
+func TranslationMatrix4x4(translationX, translationY, translationZ float32) *Matrix4x4 {
+	return &Matrix4x4{
+		[16]float32{
+			1.0, 0.0, 0.0, translationX,
+			0.0, 1.0, 0.0, translationY,
+			0.0, 0.0, 1.0, translationZ,
+			0.0, 0.0, 0.0, 1.0,
+		},
+	}
+}
+
+//func ProjectionMatrix4x4(const float &angleOfView, const float &near, const float &far, Matrix44f &M) *Matrix4x4 {
+func ProjectionMatrix4x4(angleOfView, near, far float64) *Matrix4x4 {
+	scale := float32(1 / math.Tan(angleOfView*0.5*math.Pi/180))
+	projection := NullMatrix4x4()
+	projection.Points[0] = scale
+	projection.Points[5] = scale
+	projection.Points[10] = float32(-far / (far - near))
+	projection.Points[14] = float32(-far * near / (far - near))
+	projection.Points[11] = -1
+	projection.Points[15] = 0
+	return projection
 }
 
 func (m *Matrix4x4) Dot(m2 *Matrix4x4) *Matrix4x4 {
