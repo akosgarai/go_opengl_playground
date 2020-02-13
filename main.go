8aac2e10ff49b86dcb14dbd45988380153fadc99
akosgarai <szlem01@gmail.com>
2020-02-13 13:27:53 +0100

    Main app looks good

--- main.go
+++ main.go
@@ -66,19 +66,18 @@ func initOpenGL() uint32 {
 	gl.LinkProgram(program)
 	return program
 }
-
-func generateTriangles() {
+func generateTrianglesModelCoordinates() {
 	/*
 	 * The goal is to draw triangles to the screen. The screen will contain 20 * 20 triangles.
-	 * The screen [-1, 1] -> 20 part, one part : 0.10
+	 * one part : 10 width,
 	 */
-	rows := 5
-	cols := 5
-	length := 0.40
+	rows := 100
+	cols := 100
+	length := 10.0
 	for i := 0; i <= rows; i++ {
 		for j := 0; j <= cols; j++ {
-			topX := 1.0 - (float64(j) * length)
-			topY := -1.0 + (float64(i) * length)
+			topX := (float64(j) * length)
+			topY := (float64(i) * length)
 			topZ := 0.0
 
 			triangles = append(
@@ -102,28 +101,49 @@ func main() {
 
 	// Configure global settings
 	gl.UseProgram(program)
-	/*
-	 * MPV = P * M, where P is the projection matrix and M is the model-view matrix (aka. object to world space * world space to camera space)
-	 * https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/projection-matrix-introduction
-	 */
-	uniform := [16]float32{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
-	// mvp - modelview - projection matrix
-	mvp := gl.GetUniformLocation(program, gl.Str("MVP\x00"))
-	gl.UniformMatrix4fv(mvp, 1, false, &uniform[0])
 
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
 
-	generateTriangles()
+	generateTrianglesModelCoordinates()
 
 	nowUnix := time.Now().UnixNano()
+
+	// mvp - modelview - projection matrix
+	angelOfView := float64(270)
+	near := float64(0.1)
+	far := float64(100)
+	P := primitives.ProjectionMatrix4x4(angelOfView, near, far)
+	scaleMatrix := primitives.ScaleMatrix4x4(0.01, 0.01, 0.01)
+	translationMatrix := primitives.TranslationMatrix4x4(-1, -1, -100)
+	rotationMatrix := primitives.RotationZMatrix4x4(90)
+	MV := (scaleMatrix.Dot(translationMatrix)).Dot(rotationMatrix)
+	mvpPoints := (P.Dot(MV)).Points
 
 	for !window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 		// time
 		elapsedTimeNano := time.Now().UnixNano() - nowUnix
 		time := gl.GetUniformLocation(program, gl.Str("time\x00"))
-		gl.Uniform1f(time, float32(elapsedTimeNano/100))
+		gl.Uniform1f(time, float32(elapsedTimeNano/10000000))
+		/*
+		 * MPV = P * M, where P is the projection matrix and M is the model-view matrix (aka. object to world space * world space to camera space)
+		 * P supposed to be something like this : [16]float32{
+		     0.0,0.0,0.0,0.0,
+		     0.0,0.0,0.0,0.0,
+		     0.0,0.0,0.0,0.0,
+		     0.0,0.0,0.0,0.0,
+		 }
+		 * MV supposed to be something like this : [16]float32{
+		     0.0,0.0,0.0,0.0,
+		     0.0,0.0,0.0,0.0,
+		     0.0,0.0,0.0,0.0,
+		     0.0,0.0,0.0,0.0,
+		 }
+		 * https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/projection-matrix-introduction
+		*/
+		mvp := gl.GetUniformLocation(program, gl.Str("MVP\x00"))
+		gl.UniformMatrix4fv(mvp, 1, false, &mvpPoints[0])
 
 		for _, item := range triangles {
 			item.Draw()
