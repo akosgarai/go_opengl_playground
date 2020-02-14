ead10ff634559af06c24e11988ebb87df39720c3
akosgarai <szlem01@gmail.com>
2020-02-14 16:53:55 +0100

    Add more shader solutions

--- pkg/shader/shader.go
+++ pkg/shader/shader.go
@@ -8,6 +8,25 @@ import (
 )
 
 var (
+	VertexShaderDirectOutputSource = `
+    #version 410
+    layout(location = 0) in vec3 vVertex;
+    void main()
+    {
+	gl_Position = vec4(vVertex,1);
+    }
+    ` + "\x00"
+	VertexShaderDirectOutputWithColorSource = `
+    #version 410
+    layout(location = 0) in vec3 vVertex;
+    layout(location = 1) in vec3 vColor;
+    smooth out vec4 vSmoothColor;
+    void main()
+    {
+	vSmoothColor = vec4(vColor,1);
+	gl_Position = vec4(vVertex,1);
+    }
+    ` + "\x00"
 	VertexShaderBasicSource = `
     #version 410
     layout(location = 0) in vec3 vVertex;
@@ -38,6 +57,14 @@ var (
 	gl_Position = MVP*vec4(vVertex.x, vVertex.y, z,1);
     }
     ` + "\x00"
+	FragmentShaderConstantSource = `
+    #version 410
+    layout(location=0) out vec4 vFragColor;
+    void main()
+    {
+	vFragColor = vec4(1,1,1,1);
+    }
+    ` + "\x00"
 	FragmentShaderBasicSource = `
     #version 410
     smooth in vec4 vSmoothColor;
@@ -45,6 +72,38 @@ var (
     void main()
     {
 	vFragColor = vSmoothColor;
+    }
+    ` + "\x00"
+	GeometryShaderQuadSubdivisionSource = `
+    #version 410
+    layout (triangles) in;
+    layout (triangle_strip, max_vertices=256) out;
+    uniform int sub_divisions;
+    uniform mat4 MVP;
+    void main() {
+	vec4 v0 = gl_in[0].gl_Position;
+	vec4 v1 = gl_in[1].gl_Position;
+	vec4 v2 = gl_in[2].gl_Position;
+	float dx = abs(v0.x-v2.x)/sub_divisions;
+	float dz = abs(v0.z-v1.z)/sub_divisions;
+	float x=v0.x;
+	float z=v0.z;
+	for(int j=0;j<sub_divisions*sub_divisions;j++) {
+	    gl_Position = MVP * vec4(x,0,z,1);
+	    EmitVertex();
+	    gl_Position = MVP * vec4(x,0,z+dz,1);
+	    EmitVertex();
+	    gl_Position = MVP * vec4(x+dx,0,z,1);
+	    EmitVertex();
+	    gl_Position = MVP * vec4(x+dx,0,z+dz,1);
+	    EmitVertex();
+	    EndPrimitive();
+	    x+=dx;
+	    if((j+1) %sub_divisions == 0) {
+		x=v0.x;
+		z+=dz;
+	    }
+	}
     }
     ` + "\x00"
 )
