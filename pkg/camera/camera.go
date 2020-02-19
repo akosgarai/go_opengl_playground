154d720d9605e5122fe5c9c46da7ce9c8d42da3c
akosgarai <szlem01@gmail.com>
2020-02-19 18:34:57 +0100

    Experiment - camera movement
    Due to the issue, that i can't see the reference point in the screen
    i decided to find it somehow.

--- pkg/camera/camera.go
+++ pkg/camera/camera.go
@@ -52,3 +52,56 @@ func (c *Camera) GetTransformation() *M.Matrix {
 	cameraRotationMatrix.Points[10] = c.zAxis.Z
 	return cameraRotationMatrix.Dot(*cameraTranslationMatrix)
 }
+
+// MoveCamera - updates the camera position
+func (c *Camera) MoveCamera(v V.Vector) {
+	/* cpp example code
+	   void updatePos(float dt) {
+	     Vector up = Vector(0, 1, 0), right = cross(fwd, up).normalize();
+	     up = cross(right, fwd).normalize();
+
+	     if(keys_down[W] && !keys_down[S]) {
+	       pos += fwd * speed * dt;
+	     } else if(keys_down[S] && !keys_down[W]) {
+	       pos -= fwd * speed * dt;
+	     }
+
+	     if(keys_down[D] && !keys_down[A]) {
+	       pos += right * speed * dt;
+	     } else if(keys_down[A] && !keys_down[D]) {
+	       pos -= right * speed * dt;
+	     }
+	   }
+	*/
+	c.Position = c.Position.Add(v.Normalize())
+	c.setZAxis()
+	c.setXAxis()
+	c.setYAxis()
+}
+
+// RotateCamera - updates the lookAt.
+func (c *Camera) RotateCamera(v V.Vector) {
+	/*
+	   void updateDir(int dx, int dy) {
+	     Vector y_axis = Vector(0, 1, 0), right = cross(fwd, y_axis).normalize();
+	     Vector up = cross(right, fwd).normalize();
+
+	     // Ha teljesen felfele / lefele néznénk, akkor ne forduljon át a kamera
+	     float dot_up_fwd = dot(y_axis, fwd);
+	     if(dot_up_fwd > 0.95f && dy > 0) {
+	       dy = 0;
+	     }
+	     if(dot_up_fwd < -0.95f && dy < 0) {
+	       dy = 0;
+	     }
+
+	     // Módosítsuk az nézeti irányt
+	     fwd += mouse_speed * (right * dx + up * dy);
+	     fwd = fwd.normalize();
+	   }
+	*/
+	c.LookAt = (c.LookAt.Add(v)).Normalize()
+	c.setZAxis()
+	c.setXAxis()
+	c.setYAxis()
+}
