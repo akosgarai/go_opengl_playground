6dc7069a06b4c9f6d3b207cb6f5c4251ab24076c
akosgarai <szlem01@gmail.com>
2020-02-20 00:16:15 +0100

    Remove code from the old file

--- pkg/primitives/point.go
+++ pkg/primitives/point.go
@@ -1,24 +1,10 @@
 package primitives
 
-import (
-	V "github.com/akosgarai/opengl_playground/pkg/vector"
-)
-
 type Point struct {
 	Coordinate Vector
 	Color      Vector
 }
 
 func (p *Point) SetColor(color Vector) {
-	p.Color = color
-}
-
-type PointBB struct {
-	Coordinate V.Vector
-	Color      V.Vector
-}
-
-// SetColor updates the Color of the point
-func (p *PointBB) SetColor(color V.Vector) {
 	p.Color = color
 }
