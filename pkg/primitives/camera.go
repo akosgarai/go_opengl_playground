a4810e4b90d81eabe42890f6e790c6298fab8682
akosgarai <szlem01@gmail.com>
2020-02-22 19:32:04 +0100

    Major bugfix in legacy camera

--- pkg/primitives/camera.go
+++ pkg/primitives/camera.go
@@ -23,14 +23,14 @@ func RadToDeg(rad float64) float64 {
 // YPR aka YawPitchRoll aka GetMatrixUsingYawPitchRoll creates a matrix that represents the transformations
 // the input parameters are in radians.
 func YPR(yaw, pitch, roll float64) *Matrix4x4 {
-	cosYaw = math.Cos(yaw)
-	sinYaw = math.Sin(yaw)
+	cosYaw := math.Cos(yaw)
+	sinYaw := math.Sin(yaw)
 
-	cosPitch = math.Cos(pitch)
-	sinPitch = math.Sin(pitch)
+	cosPitch := math.Cos(pitch)
+	sinPitch := math.Sin(pitch)
 
-	cosRoll = math.Cos(roll)
-	sinRoll = math.Sin(roll)
+	cosRoll := math.Cos(roll)
+	sinRoll := math.Sin(roll)
 
 	result := NullMatrix4x4()
 	result.Points[0] = float32(cosYaw*cosRoll + sinYaw*sinPitch*sinRoll)
@@ -50,7 +50,7 @@ func YPR(yaw, pitch, roll float64) *Matrix4x4 {
 // implementation of the gluLookAt function (based on the Szirmay graph book. page: 194)
 func LookAt(eye, lookAt, worldUp Vector) *Matrix4x4 {
 	w := eye.Subtract(lookAt)
-	wNorm := w.Normalize()
+	wNorm := w.Length()
 	if wNorm > EPSILON {
 		w = w.DivideScalar(wNorm)
 	} else {
@@ -58,8 +58,8 @@ func LookAt(eye, lookAt, worldUp Vector) *Matrix4x4 {
 		w.X = 0
 		w.Y = 0
 	}
-	u := up.Cross(w)
-	uNorm := u.Normalize()
+	u := worldUp.Cross(w)
+	uNorm := u.Length()
 	if uNorm > EPSILON {
 		u = u.DivideScalar(uNorm)
 	} else {
@@ -70,17 +70,17 @@ func LookAt(eye, lookAt, worldUp Vector) *Matrix4x4 {
 	v := w.Cross(u)
 	result := NullMatrix4x4()
 	// u, v, w, 0
-	result.Points[0] = u.X
-	result.Points[1] = v.X
-	result.Points[2] = w.X
-	result.Points[4] = u.Y
-	result.Points[5] = v.Y
-	result.Points[6] = w.Y
-	result.Points[8] = u.Z
-	result.Points[9] = v.Z
-	result.Points[10] = w.Z
+	result.Points[0] = float32(u.X)
+	result.Points[1] = float32(v.X)
+	result.Points[2] = float32(w.X)
+	result.Points[4] = float32(u.Y)
+	result.Points[5] = float32(v.Y)
+	result.Points[6] = float32(w.Y)
+	result.Points[8] = float32(u.Z)
+	result.Points[9] = float32(v.Z)
+	result.Points[10] = float32(w.Z)
 	result.Points[15] = 1
-	translationMatrix := TranslationMatrix4x4(-eye.X, -eye.Y, -eye.Z)
+	translationMatrix := TranslationMatrix4x4(-float32(eye.X), -float32(eye.Y), -float32(eye.Z))
 	return result.Dot(translationMatrix)
 }
 
@@ -98,9 +98,9 @@ type Camera struct {
 	cameraUpDirection    Vector
 	cameraRightDirection Vector
 	// view matrix
-	viewMatrix Matrix4x4
+	viewMatrix *Matrix4x4
 	// projection matrix
-	projectionMatrix Matrix4x4
+	projectionMatrix *Matrix4x4
 
 	freeCameraOptions struct {
 		translation Vector
@@ -121,14 +121,14 @@ type Camera struct {
 	}
 }
 
-func New() *Camera {
+func NewCamera() *Camera {
 	var cam Camera
 	cam.freeCameraOptions.translation = Vector{0, 0, 0}
 	cam.targetCameraOptions.minRy = -60
 	cam.targetCameraOptions.maxRy = 60
 	cam.targetCameraOptions.minDistance = 1
 	cam.targetCameraOptions.maxDistance = 10
-	cam.fow = 45
+	cam.fov = 45
 	cam.aspectRatio = 1.3333
 	return &cam
 }
@@ -144,12 +144,12 @@ func (c *Camera) GetFOV() float64 {
 }
 
 // GetViewMatrix returns the viewMatrix of the camera
-func (c *Camera) GetViewMatrix() Matrix4x4 {
+func (c *Camera) GetViewMatrix() *Matrix4x4 {
 	return c.viewMatrix
 }
 
 // GetProjectionMatrix returns the viewMatrix of the camera
-func (c *Camera) GetProjectionMatrix() Matrix4x4 {
+func (c *Camera) GetProjectionMatrix() *Matrix4x4 {
 	return c.projectionMatrix
 }
 
@@ -190,7 +190,7 @@ func (c *Camera) FreeCameraStrafe(amount float64) {
 }
 
 // FreeCameraLift updates the translation for the transformation (up, down directions)
-func (c *Camera) FreeCameraStrafe(amount float64) {
+func (c *Camera) FreeCameraLift(amount float64) {
 	c.freeCameraOptions.translation = c.freeCameraOptions.translation.Add(
 		c.cameraUpDirection.MultiplyScalar(amount))
 }
@@ -204,9 +204,9 @@ func (c *Camera) FreeCameraUpdate() {
 	c.position = c.position.Add(c.freeCameraOptions.translation)
 	c.freeCameraOptions.translation = Vector{0, 0, 0}
 
-	c.lookAt = rotation.MultiVector(Vector{0, 0, 1})
+	c.lookAt = *rotation.MultiVector(Vector{0, 0, 1})
 	target := c.position.Add(c.lookAt)
-	c.cameraUpDirection = rotation.MultiVector(Vector{0, 1, 0})
+	c.cameraUpDirection = *rotation.MultiVector(Vector{0, 1, 0})
 	c.cameraRightDirection = c.lookAt.Cross(c.cameraUpDirection)
 	// glm::lookAt(position, tgt, up)
 	c.viewMatrix = LookAt(c.position, target, c.cameraUpDirection)
@@ -219,14 +219,14 @@ func (c *Camera) TargetCameraUpdate() {
 		DegToRad(c.targetCameraOptions.pitch),
 		DegToRad(c.targetCameraOptions.roll))
 	T := rotation.MultiVector(Vector{0, 0, c.targetCameraOptions.distance})
-	c.position = c.targetCameraOptions.target.Add(T)
-	c.lookAt = (c.target.Subtract(c.position)).Normalize()
-	c.cameraUpDirection = rotation.MultiVector(c.UpDirection)
+	c.position = c.targetCameraOptions.target.Add(*T)
+	c.lookAt = (c.targetCameraOptions.target.Subtract(c.position)).Normalize()
+	c.cameraUpDirection = *(rotation.MultiVector(c.UpDirection))
 	c.cameraRightDirection = c.lookAt.Cross(c.cameraUpDirection)
 	c.viewMatrix = LookAt(c.position, c.targetCameraOptions.target, c.cameraUpDirection)
 }
 func (c *Camera) TargetCameraSetTarget(target Vector) {
-	c.target = target
+	c.targetCameraOptions.target = target
 	// glm::distance(vector, vector) ??
 	c.targetCameraOptions.distance = (c.position.Subtract(c.targetCameraOptions.target)).Length()
 	c.targetCameraOptions.distance = math.Max(
@@ -237,13 +237,13 @@ func (c *Camera) TargetCameraSetTarget(target Vector) {
 	c.targetCameraOptions.pitch = 0
 	if c.viewMatrix.Points[0] < 0 {
 		// m_yaw = glm::degrees((float)(M_PI - asinf(-V[2][0])) );
-		c.targetCameraOptions.yaw = RadToDeg(math.Pi - math.Asin(-c.viewMatrix[8]))
+		c.targetCameraOptions.yaw = RadToDeg(math.Pi - math.Asin(-float64(c.viewMatrix.Points[8])))
 	} else {
 		// m_yaw = glm::degrees(asinf(-V[2][0]));
-		c.targetCameraOptions.yaw = RadToDeg(math.Asin(-c.viewMatrix[8]))
+		c.targetCameraOptions.yaw = RadToDeg(math.Asin(-float64(c.viewMatrix.Points[8])))
 	}
 	// m_pitch = glm::degrees(asinf(-V[1][2]));
-	c.targetCameraOptions.pitch = RadToDeg(math.Asin(-c.viewMatrix[6]
+	c.targetCameraOptions.pitch = RadToDeg(math.Asin(-float64(c.viewMatrix.Points[6])))
 }
 func (c *Camera) TargetCameraGetTarget() Vector {
 	return c.targetCameraOptions.target
@@ -258,7 +258,7 @@ func (c *Camera) TargetCameraRotate(yaw, pitch, roll float64) {
 		c.targetCameraOptions.maxRy)
 	c.TargetCameraUpdate()
 }
-func (c *Camera) TargetCameraPan(dX, dY, float64) {
+func (c *Camera) TargetCameraPan(dX, dY float64) {
 	x := c.cameraRightDirection.MultiplyScalar(dX)
 	y := c.cameraUpDirection.MultiplyScalar(dY)
 	c.position = c.position.Add(x.Add(y))
@@ -275,7 +275,7 @@ func (c *Camera) TargetCameraZoom(amount float64) {
 			c.targetCameraOptions.maxDistance))
 	c.TargetCameraUpdate()
 }
-func (c *Camera) TargetCameraMove(dX, dY, float64) {
+func (c *Camera) TargetCameraMove(dX, dY float64) {
 	x := c.cameraRightDirection.MultiplyScalar(dX)
 	y := c.lookAt.MultiplyScalar(dY)
 	c.position = c.position.Add(x.Add(y))
