ebe8d05d5c988097392517073f1c622760ad7463
akosgarai <szlem01@gmail.com>
2020-03-03 15:47:32 +0100

    Rethink the camera

--- pkg/primitives/camera.go
+++ pkg/primitives/camera.go
@@ -1,206 +1,109 @@
 package primitives
 
-// based on the opengl dev. cookbook solution
-
 import (
 	"math"
+	"strconv"
 )
 
 type Camera struct {
-	// camera position
-	position Vector
-	// look at position (the camera is looking at lookat from it's position.)
-	lookAt Vector
-	// world up direction.
-	UpDirection Vector
+	// Camera options
 
-	fov         float64
-	aspectRatio float64
+	// Eular Angles
+	pitch float64
+	yaw   float64
 
-	cameraUpDirection    Vector
-	cameraRightDirection Vector
-	// view matrix
-	viewMatrix *Matrix4x4
-	// projection matrix
-	projectionMatrix *Matrix4x4
+	// Camera attributes
+	pos     Vector
+	front   Vector
+	up      Vector
+	right   Vector
+	worldUp Vector
+	// Projection options.
+	projectionOptions struct {
+		fov         float64
+		aspectRatio float64
 
-	freeCameraOptions struct {
-		translation Vector
-		yaw         float64
-		pitch       float64
-		roll        float64
-	}
-	targetCameraOptions struct {
-		target      Vector
-		yaw         float64
-		pitch       float64
-		roll        float64
-		minRy       float64
-		maxRy       float64
-		distance    float64
-		minDistance float64
-		maxDistance float64
+		far  float64
+		near float64
 	}
 }
 
-func NewCamera() *Camera {
-	var cam Camera
-	cam.freeCameraOptions.translation = Vector{0, 0, 0}
-	cam.targetCameraOptions.minRy = -60
-	cam.targetCameraOptions.maxRy = 60
-	cam.targetCameraOptions.minDistance = 0.1
-	cam.targetCameraOptions.maxDistance = 1000
-	cam.fov = 45
-	cam.aspectRatio = 1
+// Log returns the string representation of this object.
+func (c *Camera) Log() string {
+	logString := "Pos: Vector{" + c.pos.ToString() + "}\n"
+	logString += "worldUp: Vector{" + c.worldUp.ToString() + "}\n"
+	logString += "front: Vector{" + c.front.ToString() + "}\n"
+	logString += "up: Vector{" + c.up.ToString() + "}\n"
+	logString += "right: Vector{" + c.right.ToString() + "}\n"
+	logString += "yaw : " + strconv.FormatFloat(c.yaw, 'f', 6, 64) + "\n"
+	logString += "pitch : " + strconv.FormatFloat(c.pitch, 'f', 6, 64) + "\n"
+	logString += "ProjectionOptions:\n"
+	logString += " - fov : " + strconv.FormatFloat(c.projectionOptions.fov, 'f', 6, 64) + "\n"
+	logString += " - aspectRatio : " + strconv.FormatFloat(c.projectionOptions.aspectRatio, 'f', 6, 64) + "\n"
+	logString += " - far : " + strconv.FormatFloat(c.projectionOptions.far, 'f', 6, 64) + "\n"
+	logString += " - near : " + strconv.FormatFloat(c.projectionOptions.near, 'f', 6, 64) + "\n"
+	return logString
+}
+
+func NewCamera(position, worldUp Vector, yaw, pitch float64) *Camera {
+	cam := Camera{
+		pitch:   pitch,
+		yaw:     yaw,
+		pos:     position,
+		up:      Vector{0, 1, 0},
+		worldUp: worldUp,
+	}
+
+	cam.updateVectors()
 	return &cam
 }
 
-// GetAspectRatio returns the aspect ratio of the camera
-func (c *Camera) GetAspectRatio() float64 {
-	return c.aspectRatio
+// Walk updates the translation for the transformation (forward, back directions)
+func (c *Camera) Walk(amount float64) {
+	c.pos = c.pos.Add(c.front.MultiplyScalar(amount))
+	c.updateVectors()
 }
 
-// GetFOV returns the fov of the camera
-func (c *Camera) GetFOV() float64 {
-	return c.fov
+// Strafe updates the translation for the transformation (left, right directions)
+func (c *Camera) Strafe(amount float64) {
+	c.pos = c.pos.Add(c.front.Cross(c.up).Normalize().MultiplyScalar(amount))
+	c.updateVectors()
 }
 
-// GetViewMatrix returns the viewMatrix of the camera
-func (c *Camera) GetViewMatrix() *Matrix4x4 {
-	return c.viewMatrix
-}
-
-// GetProjectionMatrix returns the viewMatrix of the camera
-func (c *Camera) GetProjectionMatrix() *Matrix4x4 {
-	return c.projectionMatrix
-}
-
-// GetPosition returns the position of the camera
-func (c *Camera) GetPosition() Vector {
-	return c.position
-}
-
-// SetPosition updates the position of the camera
-func (c *Camera) SetPosition(newPos Vector) {
-	c.position = newPos
+// Lift updates the translation for the transformation (up, down directions)
+func (c *Camera) Lift(amount float64) {
+	c.pos = c.pos.Add(c.right.Cross(c.front).Normalize().MultiplyScalar((amount)))
+	c.updateVectors()
 }
 
 // SetupProjection creates the projection matrix and setups the fow and the aspect_ration
-func (c *Camera) SetupProjection(fov, aspRatio float64) {
-	c.fov = fov
-	c.aspectRatio = aspRatio
-	c.projectionMatrix = Perspective(float32(fov), float32(aspRatio), 0.1, 1000)
+func (c *Camera) SetupProjection(fov, aspRatio, near, far float64) {
+	c.projectionOptions.fov = fov
+	c.projectionOptions.aspectRatio = aspRatio
+	c.projectionOptions.near = near
+	c.projectionOptions.far = far
 }
 
-// FreeCameraRotate sets up the YPR values for the transformation
-func (c *Camera) FreeCameraRotate(yaw, pitch, roll float64) {
-	c.freeCameraOptions.yaw = yaw
-	c.freeCameraOptions.pitch = pitch
-	c.freeCameraOptions.roll = roll
+// GetProjectionMatrix returns the viewMatrix of the camera
+// SHOULD BE FINE : https://github.com/go-gl/mathgl/blob/95de7b3a016a8324097da95ad4417cc2caccb071/mgl32/project.go
+func (c *Camera) GetProjectionMatrix() *Matrix4x4 {
+	return Perspective(float32(c.projectionOptions.fov), float32(c.projectionOptions.aspectRatio), float32(c.projectionOptions.near), float32(c.projectionOptions.far))
 }
 
-// FreeCameraWalk updates the translation for the transformation (forward, back directions)
-func (c *Camera) FreeCameraWalk(amount float64) {
-	c.freeCameraOptions.translation = c.freeCameraOptions.translation.Add(
-		c.lookAt.MultiplyScalar(amount))
+// GetViewMatrix gets the matrix to transform from world coordinates to
+// this camera's coordinates.
+// GetViewMatrix returns the viewMatrix of the camera
+func (c *Camera) GetViewMatrix() *Matrix4x4 {
+	return LookAt_v4(c.pos, c.pos.Add(c.front), c.up)
 }
-
-// FreeCameraStrafe updates the translation for the transformation (left, right directions)
-func (c *Camera) FreeCameraStrafe(amount float64) {
-	c.freeCameraOptions.translation = c.freeCameraOptions.translation.Add(
-		c.cameraRightDirection.MultiplyScalar(amount))
-}
-
-// FreeCameraLift updates the translation for the transformation (up, down directions)
-func (c *Camera) FreeCameraLift(amount float64) {
-	c.freeCameraOptions.translation = c.freeCameraOptions.translation.Add(
-		c.cameraUpDirection.MultiplyScalar(amount))
-}
-
-// FreeCameraUpdate setup the viewMatrix based on the given freeCameraOptions
-func (c *Camera) FreeCameraUpdate() {
-	rotation := YPR(
-		c.freeCameraOptions.yaw,
-		c.freeCameraOptions.pitch,
-		c.freeCameraOptions.roll)
-	c.position = c.position.Add(c.freeCameraOptions.translation)
-	c.freeCameraOptions.translation = Vector{0, 0, 0}
-
-	c.lookAt = *rotation.MultiVector(Vector{0, 0, 1})
-	target := c.position.Add(c.lookAt)
-	c.cameraUpDirection = *rotation.MultiVector(Vector{0, 1, 0})
-	c.cameraRightDirection = c.lookAt.Cross(c.cameraUpDirection)
-	// glm::lookAt(position, tgt, up)
-	c.viewMatrix = LookAt(c.position, target, c.cameraUpDirection)
-}
-
-// TargetCameraUpdate setup the viewMatrix based on the given targetCameraOptions
-func (c *Camera) TargetCameraUpdate() {
-	rotation := YPR(
-		DegToRad(c.targetCameraOptions.yaw),
-		DegToRad(c.targetCameraOptions.pitch),
-		DegToRad(c.targetCameraOptions.roll))
-	T := rotation.MultiVector(Vector{0, 0, c.targetCameraOptions.distance})
-	c.position = c.targetCameraOptions.target.Add(*T)
-	c.lookAt = (c.targetCameraOptions.target.Subtract(c.position)).Normalize()
-	c.cameraUpDirection = *(rotation.MultiVector(c.UpDirection))
-	c.cameraRightDirection = c.lookAt.Cross(c.cameraUpDirection)
-	c.viewMatrix = LookAt(c.position, c.targetCameraOptions.target, c.cameraUpDirection)
-}
-func (c *Camera) TargetCameraSetTarget(target Vector) {
-	c.targetCameraOptions.target = target
-	// glm::distance(vector, vector) ??
-	c.targetCameraOptions.distance = (c.position.Subtract(c.targetCameraOptions.target)).Length()
-	c.targetCameraOptions.distance = math.Max(
-		c.targetCameraOptions.minDistance, math.Min(
-			c.targetCameraOptions.distance, c.targetCameraOptions.maxDistance))
-	c.viewMatrix = LookAt(c.position, c.targetCameraOptions.target, c.cameraUpDirection)
-	c.targetCameraOptions.yaw = 0
-	c.targetCameraOptions.pitch = 0
-	if c.viewMatrix.Points[0] < 0 {
-		// m_yaw = glm::degrees((float)(M_PI - asinf(-V[2][0])) );
-		c.targetCameraOptions.yaw = RadToDeg(math.Pi - math.Asin(-float64(c.viewMatrix.Points[8])))
-	} else {
-		// m_yaw = glm::degrees(asinf(-V[2][0]));
-		c.targetCameraOptions.yaw = RadToDeg(math.Asin(-float64(c.viewMatrix.Points[8])))
+func (c *Camera) updateVectors() {
+	c.front = Vector{
+		math.Cos(DegToRad(c.pitch)) * math.Cos(DegToRad(c.yaw)),
+		math.Sin(DegToRad(c.pitch)),
+		math.Cos(DegToRad(c.pitch)) * math.Sin(DegToRad(c.yaw)),
 	}
-	// m_pitch = glm::degrees(asinf(-V[1][2]));
-	c.targetCameraOptions.pitch = RadToDeg(math.Asin(-float64(c.viewMatrix.Points[6])))
-}
-func (c *Camera) TargetCameraGetTarget() Vector {
-	return c.targetCameraOptions.target
-}
-
-// TargetCameraRotate sets up the YPR values for the transformation
-func (c *Camera) TargetCameraRotate(yaw, pitch, roll float64) {
-	c.targetCameraOptions.yaw = c.targetCameraOptions.yaw + yaw
-	c.targetCameraOptions.pitch = c.targetCameraOptions.pitch + pitch
-	c.targetCameraOptions.pitch = math.Min(
-		math.Max(c.targetCameraOptions.pitch, c.targetCameraOptions.minRy),
-		c.targetCameraOptions.maxRy)
-	c.TargetCameraUpdate()
-}
-func (c *Camera) TargetCameraPan(dX, dY float64) {
-	x := c.cameraRightDirection.MultiplyScalar(dX)
-	y := c.cameraUpDirection.MultiplyScalar(dY)
-	c.position = c.position.Add(x.Add(y))
-	c.targetCameraOptions.target = c.targetCameraOptions.target.Add(x.Add(y))
-	c.TargetCameraUpdate()
-}
-func (c *Camera) TargetCameraZoom(amount float64) {
-	c.position = c.position.Add(c.lookAt.MultiplyScalar(amount))
-	c.targetCameraOptions.distance = (c.position.Subtract(c.targetCameraOptions.target)).Length()
-	c.targetCameraOptions.distance = math.Max(
-		c.targetCameraOptions.minDistance,
-		math.Min(
-			c.targetCameraOptions.distance,
-			c.targetCameraOptions.maxDistance))
-	c.TargetCameraUpdate()
-}
-func (c *Camera) TargetCameraMove(dX, dY float64) {
-	x := c.cameraRightDirection.MultiplyScalar(dX)
-	y := c.lookAt.MultiplyScalar(dY)
-	c.position = c.position.Add(x.Add(y))
-	c.targetCameraOptions.target = c.targetCameraOptions.target.Add(x.Add(y))
-	c.TargetCameraUpdate()
+	c.front = c.front.Normalize()
+	// Gram-Schmidt process to figure out right and up vectors
+	c.right = c.worldUp.Cross(c.front).Normalize()
+	c.up = c.right.Cross(c.front).Normalize()
 }
