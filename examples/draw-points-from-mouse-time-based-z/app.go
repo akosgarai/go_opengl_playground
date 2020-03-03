798504d71fcefb2edc18c1c2ad16831aaac44f4c
akosgarai <szlem01@gmail.com>
2020-03-03 18:38:53 +0100

    moveable camera to time based app

--- examples/draw-points-from-mouse-time-based-z/app.go
+++ examples/draw-points-from-mouse-time-based-z/app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/akosgarai/opengl_playground/pkg/primitives"
 	"github.com/akosgarai/opengl_playground/pkg/shader"
@@ -25,19 +26,48 @@ var (
 	colorR             = 0.0
 	colorG             = 0.0
 	colorB             = 0.0
-	modelSize          = 100
 )
 
 type Application struct {
 	Points []primitives.Point
+
+	camera           *primitives.Camera
+	cameraLastUpdate int64
+	worldUpDirection *primitives.Vector
+	moveSpeed        float64
+	epsilon          float64
+	modelSize        int
+
+	window  *glfw.Window
+	program uint32
+
+	KeyDowns map[string]bool
+}
+
+// NewApplication initializes the application.
+func NewApplication() *Application {
+	var app Application
+	app.modelSize = 10
+	app.moveSpeed = 1.0 / 1000.0
+	app.epsilon = 50.0
+	app.camera = primitives.NewCamera(primitives.Vector{0, 0, 10.0}, primitives.Vector{0, 1, 0}, -90.0, 0.0)
+	app.camera.SetupProjection(45, float64(windowWidth/windowHeight), 0.1, 1000.0)
+
+	app.cameraLastUpdate = time.Now().UnixNano()
+	app.KeyDowns = make(map[string]bool)
+	app.KeyDowns["W"] = false
+	app.KeyDowns["A"] = false
+	app.KeyDowns["S"] = false
+	app.KeyDowns["D"] = false
+	app.KeyDowns["Q"] = false
+	app.KeyDowns["E"] = false
+	return &app
 }
 
 // AddPoint inserts a new point to the points.
 func (a *Application) AddPoint(point primitives.Point) {
 	a.Points = append(a.Points, point)
 }
-
-var app Application
 
 // Basic function for glfw initialization.
 func initGlfw() *glfw.Window {
@@ -69,7 +99,7 @@ func initOpenGL() uint32 {
 	version := gl.GoStr(gl.GetString(gl.VERSION))
 	fmt.Println("OpenGL version", version)
 
-	vertexShader, err := shader.CompileShader(shader.VertexShaderPointWithColorMVPSource, gl.VERTEX_SHADER)
+	vertexShader, err := shader.CompileShader(shader.VertexShaderPointWithColorModelViewProjectionSource, gl.VERTEX_SHADER)
 	if err != nil {
 		panic(err)
 	}
@@ -90,10 +120,10 @@ func initOpenGL() uint32 {
 * - if the left mouse button is not pressed, and the button is just released, App.AddPoint(), clean up the temp.point.
 * - if the button is just pressed, set the point that needs to be added.
  */
-func mouseHandler(window *glfw.Window) {
-	x, y := window.GetCursorPos()
+func (a *Application) MouseHandler() {
+	x, y := a.window.GetCursorPos()
 
-	if window.GetMouseButton(glfw.MouseButtonMiddle) == glfw.Press {
+	if a.window.GetMouseButton(glfw.MouseButtonMiddle) == glfw.Press {
 		if !mouseButtonPressed {
 			mousePositionX = x
 			mousePositionY = y
@@ -103,10 +133,10 @@ func mouseHandler(window *glfw.Window) {
 		if mouseButtonPressed {
 			mouseButtonPressed = false
 			x, y := convertMouseCoordinates()
-			positionVector := primitives.Vector{x, y, 1.0}
-			scalarPart := float64(modelSize / 2)
+			positionVector := primitives.Vector{x, y, -1.0}
+			scalarPart := float64(a.modelSize / 2)
 			modelPositionVector := (positionVector.MultiplyScalar(scalarPart)).AddScalar(scalarPart)
-			app.AddPoint(
+			a.AddPoint(
 				primitives.Point{
 					modelPositionVector,
 					primitives.Vector{colorR, colorG, colorB},
@@ -116,29 +146,98 @@ func mouseHandler(window *glfw.Window) {
 }
 
 // Key handler function. it supports the debug option. (print out the points of the app)
-func keyHandler(window *glfw.Window) {
-	if window.GetKey(glfw.KeyD) == glfw.Press {
+func (a *Application) KeyHandler() {
+	if a.window.GetKey(glfw.KeyH) == glfw.Press {
 		if !DebugPrint {
 			DebugPrint = true
-			fmt.Println(app.Points)
+			fmt.Print("app.Points:\n")
+			fmt.Println(a.Points)
+			fmt.Printf("app.camera: %s\n", a.camera.Log())
 		}
 	} else {
 		DebugPrint = false
 	}
-	if window.GetKey(glfw.KeyR) == glfw.Press {
+	if a.window.GetKey(glfw.KeyR) == glfw.Press {
 		colorR = 1
 	} else {
 		colorR = 0
 	}
-	if window.GetKey(glfw.KeyG) == glfw.Press {
+	if a.window.GetKey(glfw.KeyG) == glfw.Press {
 		colorG = 1
 	} else {
 		colorG = 0
 	}
-	if window.GetKey(glfw.KeyB) == glfw.Press {
+	if a.window.GetKey(glfw.KeyB) == glfw.Press {
 		colorB = 1
 	} else {
 		colorB = 0
+	}
+	if a.window.GetKey(glfw.KeyW) == glfw.Press {
+		a.KeyDowns["W"] = true
+	} else {
+		a.KeyDowns["W"] = false
+	}
+	if a.window.GetKey(glfw.KeyA) == glfw.Press {
+		a.KeyDowns["A"] = true
+	} else {
+		a.KeyDowns["A"] = false
+	}
+	if a.window.GetKey(glfw.KeyS) == glfw.Press {
+		a.KeyDowns["S"] = true
+	} else {
+		a.KeyDowns["S"] = false
+	}
+	if a.window.GetKey(glfw.KeyD) == glfw.Press {
+		a.KeyDowns["D"] = true
+	} else {
+		a.KeyDowns["D"] = false
+	}
+	if a.window.GetKey(glfw.KeyQ) == glfw.Press {
+		a.KeyDowns["Q"] = true
+	} else {
+		a.KeyDowns["Q"] = false
+	}
+	if a.window.GetKey(glfw.KeyE) == glfw.Press {
+		a.KeyDowns["E"] = true
+	} else {
+		a.KeyDowns["E"] = false
+	}
+	//calculate delta
+	nowUnix := time.Now().UnixNano()
+	delta := nowUnix - a.cameraLastUpdate
+	moveTime := float64(delta / int64(time.Millisecond))
+	// if the camera has been updated recently, we can skip now
+	if a.epsilon > moveTime {
+		return
+	}
+	a.cameraLastUpdate = nowUnix
+	// Move camera
+	forward := 0.0
+	if a.KeyDowns["W"] && !a.KeyDowns["S"] {
+		forward = a.moveSpeed * moveTime
+	} else if a.KeyDowns["S"] && !a.KeyDowns["W"] {
+		forward = -a.moveSpeed * moveTime
+	}
+	if forward != 0 {
+		a.camera.Walk(forward)
+	}
+	horisontal := 0.0
+	if a.KeyDowns["A"] && !a.KeyDowns["D"] {
+		horisontal = -a.moveSpeed * moveTime
+	} else if a.KeyDowns["D"] && !a.KeyDowns["A"] {
+		horisontal = a.moveSpeed * moveTime
+	}
+	if horisontal != 0 {
+		a.camera.Strafe(horisontal)
+	}
+	vertical := 0.0
+	if a.KeyDowns["Q"] && !a.KeyDowns["E"] {
+		vertical = -a.moveSpeed * moveTime
+	} else if a.KeyDowns["E"] && !a.KeyDowns["Q"] {
+		vertical = a.moveSpeed * moveTime
+	}
+	if vertical != 0 {
+		a.camera.Lift(vertical)
 	}
 }
 func convertMouseCoordinates() (float64, float64) {
@@ -148,25 +247,23 @@ func convertMouseCoordinates() (float64, float64) {
 	y := (halfHeight - mousePositionY) / (halfHeight)
 	return x, y
 }
-func buildVAO() []float32 {
+func (a *Application) buildVAO() []float32 {
 	var vao []float32
-	for _, item := range app.Points {
+	for _, item := range a.Points {
 		vao = append(vao, float32(item.Coordinate.X))
 		vao = append(vao, float32(item.Coordinate.Y))
 		vao = append(vao, float32(item.Coordinate.Z))
-	}
-	for _, item := range app.Points {
 		vao = append(vao, float32(item.Color.X))
 		vao = append(vao, float32(item.Color.Y))
 		vao = append(vao, float32(item.Color.Z))
 	}
 	return vao
 }
-func Draw() {
-	if len(app.Points) < 1 {
+func (a *Application) Draw() {
+	if len(a.Points) < 1 {
 		return
 	}
-	points := buildVAO()
+	points := a.buildVAO()
 	var vertexBufferObject uint32
 	gl.GenBuffers(1, &vertexBufferObject)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
@@ -179,22 +276,24 @@ func Draw() {
 	gl.BindVertexArray(vertexArrayObject)
 	// setup points
 	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 4*3, gl.PtrOffset(0))
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 4*6, gl.PtrOffset(0))
 	gl.EnableVertexAttribArray(1)
-	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 4*3, gl.PtrOffset(4*3*len(app.Points)))
+	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 4*6, gl.PtrOffset(4*3))
 
 	gl.BindVertexArray(vertexArrayObject)
-	gl.DrawArrays(gl.POINTS, 0, int32(len(app.Points)))
+	gl.DrawArrays(gl.POINTS, 0, int32(len(a.Points)))
 }
 
 func main() {
 	runtime.LockOSThread()
 
-	window := initGlfw()
-	defer glfw.Terminate()
-	program := initOpenGL()
+	app := NewApplication()
 
-	gl.UseProgram(program)
+	app.window = initGlfw()
+	defer glfw.Terminate()
+	app.program = initOpenGL()
+
+	gl.UseProgram(app.program)
 
 	gl.Enable(gl.PROGRAM_POINT_SIZE)
 
@@ -205,55 +304,24 @@ func main() {
 
 	gl.ClearColor(0.3, 0.3, 0.3, 1.0)
 
-	// based on the following tutorial: http://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/
-	// I have to define and multiply the following matrixes:
-	// - Projection 'glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) width / (float)height, 0.1f, 100.0f);'
-	angleOfView := float64(180)
-	near := float64(0.1)
-	far := float64(100)
-	ProjectionMatrix := primitives.ProjectionMatrix4x4(angleOfView, near, far)
-	fmt.Println(ProjectionMatrix)
-	// - Camera matrix
-	/* based on the following doc: https://community.khronos.org/t/about-glm-lookat-function/74506
-	 */
-	cameraPosition := primitives.Vector{50, 50, 50}
-	cameraLookAt := primitives.Vector{50, 50, 0}
-	worldUpDirection := primitives.Vector{0, 1, 0}
-	zAxis := (cameraPosition.Subtract(cameraLookAt)).Normalize()
-	xAxis := ((worldUpDirection.Normalize()).Cross(zAxis)).Normalize()
-	yAxis := zAxis.Cross(xAxis)
-	cameraTranslationMatrix := primitives.TranslationMatrixT4x4(
-		-float32(cameraPosition.X),
-		-float32(cameraPosition.Y),
-		-float32(cameraPosition.Z))
-	cameraRotationMatrix := primitives.UnitMatrix4x4()
-	cameraRotationMatrix.Points[0] = float32(xAxis.X)
-	cameraRotationMatrix.Points[4] = float32(xAxis.Y)
-	cameraRotationMatrix.Points[8] = float32(xAxis.Z)
-	cameraRotationMatrix.Points[1] = float32(yAxis.X)
-	cameraRotationMatrix.Points[5] = float32(yAxis.Y)
-	cameraRotationMatrix.Points[9] = float32(yAxis.Z)
-	cameraRotationMatrix.Points[2] = float32(zAxis.X)
-	cameraRotationMatrix.Points[6] = float32(zAxis.Y)
-	cameraRotationMatrix.Points[10] = float32(zAxis.Z)
-	CameraMatrix := cameraRotationMatrix.Dot(cameraTranslationMatrix)
-	fmt.Println(CameraMatrix)
-	// - Model matrix (the scale)
-	translationMatrix := primitives.TranslationMatrixT4x4(-50, -50, -50)
-	scaleMatrix := primitives.ScaleMatrix4x4(1/50.0, 1/50.0, 1/50.0)
-	ModelMatrix := translationMatrix.Dot(scaleMatrix)
-	// mvp = projection * camera * model
-	MVP := ((ProjectionMatrix.Dot(CameraMatrix)).Dot(ModelMatrix)).Points
-	mvpLocation := gl.GetUniformLocation(program, gl.Str("MVP\x00"))
-	gl.UniformMatrix4fv(mvpLocation, 1, false, &MVP[0])
-	fmt.Println(MVP)
+	modelLocation := gl.GetUniformLocation(app.program, gl.Str("model\x00"))
+	viewLocation := gl.GetUniformLocation(app.program, gl.Str("view\x00"))
+	projectionLocation := gl.GetUniformLocation(app.program, gl.Str("projection\x00"))
 
-	for !window.ShouldClose() {
+	for !app.window.ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		keyHandler(window)
-		mouseHandler(window)
-		Draw()
+		app.KeyHandler()
+		app.MouseHandler()
+
+		M := primitives.UnitMatrix4x4().GetMatrix()
+		gl.UniformMatrix4fv(modelLocation, 1, false, &M[0])
+		V := app.camera.GetViewMatrix().GetMatrix()
+		gl.UniformMatrix4fv(viewLocation, 1, false, &V[0])
+		P := app.camera.GetProjectionMatrix().GetMatrix()
+		gl.UniformMatrix4fv(projectionLocation, 1, false, &P[0])
+
+		app.Draw()
 		glfw.PollEvents()
-		window.SwapBuffers()
+		app.window.SwapBuffers()
 	}
 }
