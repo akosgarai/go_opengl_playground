8faee4453bc8d5792f8f381fcb3cd2f7eca3621a
akosgarai <szlem01@gmail.com>
2020-03-03 16:09:00 +0100

    Tests for some functions

---
+++ pkg/primitives/camera_test.go
@@ -0,0 +1,20 @@
+package primitives
+
+import (
+	"testing"
+)
+
+func TestNewCamera(t *testing.T) {
+}
+
+// test getters
+func TestGetAspectRatio(t *testing.T) {
+}
+func TestGetFOV(t *testing.T) {
+}
+func TestGetViewMatrix(t *testing.T) {
+}
+func TestGetProjectionMatrix(t *testing.T) {
+}
+func TestGetPosition(t *testing.T) {
+}
