41c0154027cbab68c7b5e6d23862ebbb8ce75d0b
akosgarai <szlem01@gmail.com>
2020-03-03 19:34:40 +0100

    Remove unused shaders

--- pkg/shader/shader.go
+++ pkg/shader/shader.go
@@ -31,20 +31,6 @@ var (
 	vSmoothColor = vec4(vColor,1);
     }
     ` + "\x00"
-	VertexShaderPointWithColorMVPSource = `
-    #version 410
-    layout(location = 0) in vec3 vVertex;
-    layout(location = 1) in vec3 vColor;
-    const float pointSize = 20.0;
-    smooth out vec4 vSmoothColor;
-    uniform mat4 MVP;
-    void main()
-    {
-	gl_Position = MVP*vec4(vVertex,1);
-	gl_PointSize = pointSize;
-	vSmoothColor = vec4(vColor,1);
-    }
-    ` + "\x00"
 	VertexShaderPointWithColorModelViewProjectionSource = `
     #version 410
     layout(location = 0) in vec3 vVertex;
@@ -66,17 +52,6 @@ var (
     layout(location = 0) in vec3 vVertex;
     void main()
     {
-	gl_Position = vec4(vVertex,1);
-    }
-    ` + "\x00"
-	VertexShaderDirectOutputWithColorSource = `
-    #version 410
-    layout(location = 0) in vec3 vVertex;
-    layout(location = 1) in vec3 vColor;
-    smooth out vec4 vSmoothColor;
-    void main()
-    {
-	vSmoothColor = vec4(vColor,1);
 	gl_Position = vec4(vVertex,1);
     }
     ` + "\x00"
