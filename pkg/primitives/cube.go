0d964b5fefc127b4fd19ffcebc3ab86f6e070baa
akosgarai <szlem01@gmail.com>
2020-03-03 16:08:17 +0100

    Add helper function for make cube testable

--- pkg/primitives/cube.go
+++ pkg/primitives/cube.go
@@ -50,8 +50,7 @@ func (c *Cube) sideByPointToVao(currentVao []float32, pa, pb, pc, pd Point) []fl
 	currentVao = c.appendPointToVao(currentVao, pd)
 	return currentVao
 }
-
-func (c *Cube) buildVao() uint32 {
+func (c *Cube) setupVao() []float32 {
 	var points []float32
 
 	// back
@@ -67,6 +66,60 @@ func (c *Cube) buildVao() uint32 {
 	// bottom
 	points = c.sideByPointToVao(points, c.A, c.B, c.G, c.H)
 
+	return points
+}
+func (c *Cube) setupVaoWithColor() []float32 {
+	var points []float32
+
+	// back - 1,0,0
+	red := Vector{1.0, 0.0, 0.0}
+	c.A.Color = red
+	c.B.Color = red
+	c.C.Color = red
+	c.D.Color = red
+	points = c.sideByPointToVao(points, c.A, c.B, c.C, c.D)
+	// right - 0,1,0
+	green := Vector{0.0, 1.0, 0.0}
+	c.B.Color = green
+	c.G.Color = green
+	c.F.Color = green
+	c.C.Color = green
+	points = c.sideByPointToVao(points, c.B, c.G, c.F, c.C)
+	// top - 0,0,1
+	blue := Vector{0.0, 0.0, 1.0}
+	c.C.Color = blue
+	c.F.Color = blue
+	c.E.Color = blue
+	c.D.Color = blue
+	points = c.sideByPointToVao(points, c.C, c.F, c.E, c.D)
+	// front 0,1,1
+	redI := Vector{0.0, 1.0, 1.0}
+	c.G.Color = redI
+	c.F.Color = redI
+	c.E.Color = redI
+	c.H.Color = redI
+	points = c.sideByPointToVao(points, c.G, c.F, c.E, c.H)
+	// left 1,0,1
+	greenI := Vector{1.0, 0.0, 1.0}
+	c.E.Color = greenI
+	c.D.Color = greenI
+	c.A.Color = greenI
+	c.H.Color = greenI
+	points = c.sideByPointToVao(points, c.E, c.D, c.A, c.H)
+	// bottom 1,1,0
+	blueI := Vector{1.0, 1.0, 0.0}
+	c.A.Color = blueI
+	c.B.Color = blueI
+	c.G.Color = blueI
+	c.H.Color = blueI
+	points = c.sideByPointToVao(points, c.A, c.B, c.G, c.H)
+
+	return points
+}
+
+func (c *Cube) buildVao() uint32 {
+	points := c.setupVaoWithColor()
+
 	var vertexBufferObject uint32
 	gl.GenBuffers(1, &vertexBufferObject)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
@@ -78,11 +131,11 @@ func (c *Cube) buildVao() uint32 {
 	gl.GenVertexArrays(1, &vertexArrayObject)
 	gl.BindVertexArray(vertexArrayObject)
 	// setup points
-	gl.EnableVertexAttribArray(0)
 	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 4*6, gl.PtrOffset(0))
+	gl.EnableVertexAttribArray(0)
 	// setup color
-	gl.EnableVertexAttribArray(1)
 	gl.VertexAttribPointer(1, 3, gl.FLOAT, false, 4*6, gl.PtrOffset(4*3))
+	gl.EnableVertexAttribArray(1)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
 
 	return vertexArrayObject
