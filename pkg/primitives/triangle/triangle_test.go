97f2751dbd476b75fd32d9f39bdb8ae4e569af4f
akosgarai <szlem01@gmail.com>
2020-03-14 13:50:22 +0100

    Documentation & skeleton for tests

--- pkg/primitives/triangle/triangle_test.go
+++ pkg/primitives/triangle/triangle_test.go
@@ -1,1 +1,15 @@
 package triangle
+
+import (
+	"testing"
+)
+
+func TestNewTriangle(t *testing.T) {
+	t.Skip()
+}
+func TestSetColor(t *testing.T) {
+	t.Skip()
+}
+func TestSetupVao(t *testing.T) {
+	t.Skip()
+}
