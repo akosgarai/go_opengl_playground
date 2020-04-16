479aac2db7bb81057fa6e71f78d3dbc9c029659c
akosgarai <szlem01@gmail.com>
2020-04-16 15:11:18 +0200

    Use vao in cube

--- examples/callbacks/primitives/cube.go
+++ examples/callbacks/primitives/cube.go
@@ -14,10 +14,12 @@ type Cube struct {
 	F Point // top-right-front
 	G Point // bottom-right-front
 	H Point // bottom-left-front
+
+	vao *VAO
 }
 
 func NewCubeByPoints(a, b, c, d, e, f, g, h Point) *Cube {
-	return &Cube{a, b, c, d, e, f, g, h}
+	return &Cube{a, b, c, d, e, f, g, h, NewVAO()}
 }
 func NewCubeByVectorAndLength(in mgl32.Vec3, sideLength float32) *Cube {
 	color1 := mgl32.Vec3{1, 0, 0}
@@ -30,103 +32,86 @@ func NewCubeByVectorAndLength(in mgl32.Vec3, sideLength float32) *Cube {
 	f := Point{e.Coordinate.Add(mgl32.Vec3{sideLength, 0, 0}), color2}
 	g := Point{b.Coordinate.Add(mgl32.Vec3{0, 0, sideLength}), color2}
 	h := Point{a.Coordinate.Add(mgl32.Vec3{0, 0, sideLength}), color2}
-	return &Cube{a, b, c, d, e, f, g, h}
+	return &Cube{a, b, c, d, e, f, g, h, NewVAO()}
 }
 
-func (c *Cube) appendPointToVao(currentVao []float32, p Point) []float32 {
-	currentVao = append(currentVao, p.Coordinate.X())
-	currentVao = append(currentVao, p.Coordinate.Y())
-	currentVao = append(currentVao, p.Coordinate.Z())
-	currentVao = append(currentVao, p.Color.X())
-	currentVao = append(currentVao, p.Color.Y())
-	currentVao = append(currentVao, p.Color.Z())
-	return currentVao
+func (c *Cube) sideByPointToVao(pa, pb, pc, pd Point) {
+	c.vao.AppendPoint(pa)
+	c.vao.AppendPoint(pb)
+	c.vao.AppendPoint(pc)
+	c.vao.AppendPoint(pa)
+	c.vao.AppendPoint(pc)
+	c.vao.AppendPoint(pd)
 }
-func (c *Cube) sideByPointToVao(currentVao []float32, pa, pb, pc, pd Point) []float32 {
-	currentVao = c.appendPointToVao(currentVao, pa)
-	currentVao = c.appendPointToVao(currentVao, pb)
-	currentVao = c.appendPointToVao(currentVao, pc)
-	currentVao = c.appendPointToVao(currentVao, pa)
-	currentVao = c.appendPointToVao(currentVao, pc)
-	currentVao = c.appendPointToVao(currentVao, pd)
-	return currentVao
-}
-func (c *Cube) setupVao() []float32 {
-	var points []float32
-
+func (c *Cube) setupVao() {
 	// back
-	points = c.sideByPointToVao(points, c.A, c.B, c.C, c.D)
+	c.sideByPointToVao(c.A, c.B, c.C, c.D)
 	// right
-	points = c.sideByPointToVao(points, c.B, c.G, c.F, c.C)
+	c.sideByPointToVao(c.B, c.G, c.F, c.C)
 	// top
-	points = c.sideByPointToVao(points, c.C, c.F, c.E, c.D)
+	c.sideByPointToVao(c.C, c.F, c.E, c.D)
 	// front
-	points = c.sideByPointToVao(points, c.G, c.F, c.E, c.H)
+	c.sideByPointToVao(c.G, c.F, c.E, c.H)
 	// left
-	points = c.sideByPointToVao(points, c.E, c.D, c.A, c.H)
+	c.sideByPointToVao(c.E, c.D, c.A, c.H)
 	// bottom
-	points = c.sideByPointToVao(points, c.A, c.B, c.G, c.H)
-
-	return points
+	c.sideByPointToVao(c.A, c.B, c.G, c.H)
 }
-func (c *Cube) setupVaoWithColor() []float32 {
-	var points []float32
-
+func (c *Cube) setupVaoWithColor() {
 	// back - 1,0,0
 	red := mgl32.Vec3{1.0, 0.0, 0.0}
 	c.A.Color = red
 	c.B.Color = red
 	c.C.Color = red
 	c.D.Color = red
-	points = c.sideByPointToVao(points, c.A, c.B, c.C, c.D)
+	c.sideByPointToVao(c.A, c.B, c.C, c.D)
 	// right - 0,1,0
 	green := mgl32.Vec3{0.0, 1.0, 0.0}
 	c.B.Color = green
 	c.G.Color = green
 	c.F.Color = green
 	c.C.Color = green
-	points = c.sideByPointToVao(points, c.B, c.G, c.F, c.C)
+	c.sideByPointToVao(c.B, c.G, c.F, c.C)
 	// top - 0,0,1
 	blue := mgl32.Vec3{0.0, 0.0, 1.0}
 	c.C.Color = blue
 	c.F.Color = blue
 	c.E.Color = blue
 	c.D.Color = blue
-	points = c.sideByPointToVao(points, c.C, c.F, c.E, c.D)
+	c.sideByPointToVao(c.C, c.F, c.E, c.D)
 	// front 0,1,1
 	redI := mgl32.Vec3{0.0, 1.0, 1.0}
 	c.G.Color = redI
 	c.F.Color = redI
 	c.E.Color = redI
 	c.H.Color = redI
-	points = c.sideByPointToVao(points, c.G, c.F, c.E, c.H)
+	c.sideByPointToVao(c.G, c.F, c.E, c.H)
 	// left 1,0,1
 	greenI := mgl32.Vec3{1.0, 0.0, 1.0}
 	c.E.Color = greenI
 	c.D.Color = greenI
 	c.A.Color = greenI
 	c.H.Color = greenI
-	points = c.sideByPointToVao(points, c.E, c.D, c.A, c.H)
+	c.sideByPointToVao(c.E, c.D, c.A, c.H)
 	// bottom 1,1,0
 	blueI := mgl32.Vec3{1.0, 1.0, 0.0}
 	c.A.Color = blueI
 	c.B.Color = blueI
 	c.G.Color = blueI
 	c.H.Color = blueI
-	points = c.sideByPointToVao(points, c.A, c.B, c.G, c.H)
-
-	return points
+	c.sideByPointToVao(c.A, c.B, c.G, c.H)
 }
 
 func (c *Cube) buildVao() uint32 {
-	points := c.setupVaoWithColor()
+	c.vao.Clear()
+	c.setupVaoWithColor()
 
 	var vertexBufferObject uint32
 	gl.GenBuffers(1, &vertexBufferObject)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
 	// a 32-bit float has 4 bytes, so we are saying the size of the buffer,
 	// in bytes, is 4 times the number of points
-	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
+	gl.BufferData(gl.ARRAY_BUFFER, 4*len(c.vao.Get()), gl.Ptr(c.vao.Get()), gl.STATIC_DRAW)
 
 	var vertexArrayObject uint32
 	gl.GenVertexArrays(1, &vertexArrayObject)
@@ -143,7 +128,6 @@ func (c *Cube) buildVao() uint32 {
 }
 
 func (c *Cube) Draw() {
-	vertexArrayObject := c.buildVao()
-	gl.BindVertexArray(vertexArrayObject)
+	c.buildVao()
 	gl.DrawArrays(gl.TRIANGLES, 0, 3*12)
 }
