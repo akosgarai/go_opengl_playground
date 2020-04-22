65301c26f74639e71369e03229dfbe6af11e7b5f
akosgarai <szlem01@gmail.com>
2020-04-22 23:04:58 +0200

    New appedn function to vao

--- examples/house/primitives/vao.go
+++ examples/house/primitives/vao.go
@@ -1,5 +1,9 @@
 package primitives
 
+import (
+	"github.com/go-gl/mathgl/mgl32"
+)
+
 type VAO struct {
 	vao []float32
 }
@@ -30,6 +34,15 @@ func (v *VAO) AppendSquarePoints(pa, pb, pc, pd Point) {
 	v.AppendPoint(pa)
 	v.AppendPoint(pc)
 	v.AppendPoint(pd)
+}
+
+func (v *VAO) AppendVectors(v1, v2 mgl32.Vec3) {
+	v.vao = append(v.vao, v1.X())
+	v.vao = append(v.vao, v1.Y())
+	v.vao = append(v.vao, v1.Z())
+	v.vao = append(v.vao, v2.X())
+	v.vao = append(v.vao, v2.Y())
+	v.vao = append(v.vao, v2.Z())
 }
 func (v *VAO) Get() []float32 {
 	return v.vao
