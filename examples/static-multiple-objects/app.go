c01681a78edd6eeeec7e510713bbf58cef99e5e4
akosgarai <szlem01@gmail.com>
2020-04-29 00:34:14 +0200

    Update static-multiple-objects application

--- examples/static-multiple-objects/app.go
+++ examples/static-multiple-objects/app.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"runtime"
 
-	mat "github.com/akosgarai/opengl_playground/pkg/primitives/matrix"
+	"github.com/akosgarai/opengl_playground/pkg/application"
 	sq "github.com/akosgarai/opengl_playground/pkg/primitives/square"
 	tr "github.com/akosgarai/opengl_playground/pkg/primitives/triangle"
-	vec "github.com/akosgarai/opengl_playground/pkg/primitives/vector"
 	"github.com/akosgarai/opengl_playground/pkg/shader"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
 const (
@@ -21,88 +20,54 @@ const (
 )
 
 var (
-	triangle = tr.NewTriangle(
-		vec.Vector{-0.75, 0.75, 0}, // top
-		vec.Vector{-0.75, 0.25, 0}, // left
-		vec.Vector{-0.25, 0.25, 0}, // right
-	)
-	square = sq.NewSquare(
-		vec.Vector{0.25, -0.25, 0}, // top-left
-		vec.Vector{0.25, -0.75, 0}, // bottom-left
-		vec.Vector{0.75, -0.75, 0}, // bottom-right
-		vec.Vector{0.75, -0.25, 0}, // top-right
-	)
+	triangleCoordinates = [3]mgl32.Vec3{
+		mgl32.Vec3{-0.75, 0.75, 0}, // top
+		mgl32.Vec3{-0.75, 0.25, 0}, // left
+		mgl32.Vec3{-0.25, 0.25, 0}, // right
+	}
+	triangleColors = [3]mgl32.Vec3{
+		mgl32.Vec3{0, 1, 0}, // top
+		mgl32.Vec3{0, 1, 0}, // left
+		mgl32.Vec3{0, 1, 0}, // right
+	}
+	squareCoordinates = [4]mgl32.Vec3{
+		mgl32.Vec3{0.25, -0.25, 0}, // top-left
+		mgl32.Vec3{0.25, -0.75, 0}, // bottom-left
+		mgl32.Vec3{0.75, -0.75, 0}, // bottom-right
+		mgl32.Vec3{0.75, -0.25, 0}, // top-right
+	}
+	squareColors = [4]mgl32.Vec3{
+		mgl32.Vec3{0, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+
+	app *application.Application
 )
-
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(fmt.Errorf("could not initialize glfw: %v", err))
-	}
-
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.Resizable, glfw.True)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(windowWidth, windowHeight, windowTitle, nil, nil)
-
-	if err != nil {
-		panic(fmt.Errorf("could not create opengl renderer: %v", err))
-	}
-
-	window.MakeContextCurrent()
-
-	return window
-}
-
-func initOpenGL() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Println("OpenGL version", version)
-
-	vertexShader, err := shader.CompileShader(shader.VertexShaderBasicSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-	fragmentShader, err := shader.CompileShader(shader.FragmentShaderBasicSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	program := gl.CreateProgram()
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.LinkProgram(program)
-	return program
-}
 
 func main() {
 	runtime.LockOSThread()
 
-	window := initGlfw()
+	app = application.New()
+	app.SetWindow(application.InitGlfw(windowWidth, windowHeight, windowTitle))
 	defer glfw.Terminate()
-	program := initOpenGL()
+	application.InitOpenGL()
 
-	gl.UseProgram(program)
+	shaderProgram := shader.NewShader("examples/static-multiple-objects/vertexshader.vert", "examples/static-multiple-objects/fragmentshader.frag")
 
-	mvpLocation := gl.GetUniformLocation(program, gl.Str("MVP\x00"))
-
-	P := mat.UnitMatrix()
-	MV := mat.UnitMatrix()
-	mvp := P.Dot(MV).GetMatrix()
-	gl.UniformMatrix4fv(mvpLocation, 1, false, &mvp[0])
+	triangle := tr.NewTriangle(triangleCoordinates, triangleColors, shaderProgram)
+	app.AddItem(triangle)
+	square := sq.NewSquare(squareCoordinates, squareColors, shaderProgram)
+	app.AddItem(square)
 
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
 
-	for !window.ShouldClose() {
+	for !app.GetWindow().ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		triangle.Draw()
-		square.Draw()
+		app.Draw()
 		glfw.PollEvents()
-		window.SwapBuffers()
+		app.GetWindow().SwapBuffers()
 	}
 }
