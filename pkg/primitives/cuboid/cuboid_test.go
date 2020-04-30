d4e79e7d1d49449df4fefb6832d78ff9a98e0322
akosgarai <szlem01@gmail.com>
2020-04-30 20:53:59 +0200

    Empty tests for cuboid

--- pkg/primitives/cuboid/cuboid_test.go
+++ pkg/primitives/cuboid/cuboid_test.go
@@ -7,3 +7,42 @@ import (
 func TestNew(t *testing.T) {
 	t.Skip()
 }
+func TestLog(t *testing.T) {
+	t.Skip()
+}
+func TestSetColor(t *testing.T) {
+	t.Skip()
+}
+func TestSetIndexColor(t *testing.T) {
+	t.Skip()
+}
+func TestSetSideColor(t *testing.T) {
+	t.Skip()
+}
+func TestSetDirection(t *testing.T) {
+	t.Skip()
+}
+func TestSetIndexDirection(t *testing.T) {
+	t.Skip()
+}
+func TestSetSpeed(t *testing.T) {
+	t.Skip()
+}
+func TestSetPrecision(t *testing.T) {
+	t.Skip()
+}
+func TestSetupVao(t *testing.T) {
+	t.Skip()
+}
+func TestBuildVao(t *testing.T) {
+	t.Skip()
+}
+func TestDraw(t *testing.T) {
+	t.Skip()
+}
+func TestDrawWithUniforms(t *testing.T) {
+	t.Skip()
+}
+func TestUpdate(t *testing.T) {
+	t.Skip()
+}
