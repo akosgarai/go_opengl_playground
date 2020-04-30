874b7c979fc749eb399220c5631e407d7a2617f7
akosgarai <szlem01@gmail.com>
2020-05-01 01:08:55 +0200

    Remove redundant app

--- examples/shapes-with-camera/app.go
+++ examples/shapes-with-camera/app.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"runtime"
 	"time"
 
+	"github.com/akosgarai/opengl_playground/pkg/application"
 	"github.com/akosgarai/opengl_playground/pkg/primitives/camera"
-	"github.com/akosgarai/opengl_playground/pkg/primitives/cube"
-	sp "github.com/akosgarai/opengl_playground/pkg/primitives/sphere"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/cuboid"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/rectangle"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/sphere"
 	trans "github.com/akosgarai/opengl_playground/pkg/primitives/transformations"
-	vec "github.com/akosgarai/opengl_playground/pkg/primitives/vector"
 	"github.com/akosgarai/opengl_playground/pkg/shader"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
@@ -21,295 +21,191 @@ const (
 	windowWidth  = 800
 	windowHeight = 800
 	windowTitle  = "Example - shapes with camera"
+
+	FORWARD  = glfw.KeyW // Go forward
+	BACKWARD = glfw.KeyS // Go backward
+	LEFT     = glfw.KeyA // Go left
+	RIGHT    = glfw.KeyD // Go right
+	UP       = glfw.KeyQ
+	DOWN     = glfw.KeyE
+
+	moveSpeed = 0.005
 )
 
 var (
-	DebugPrint = false
+	app *application.Application
+
+	lastUpdate           int64
+	cameraDistance       = 0.1
+	cameraDirectionSpeed = float32(0.500)
 )
 
-type Application struct {
-	sphere               *sp.Sphere
-	cube                 *cube.Cube
-	cubePosition         vec.Vector
-	camera               *camera.Camera
-	cameraDirection      float64
-	cameraDirectionSpeed float64
-	cameraLastUpdate     int64
-	worldUpDirection     *vec.Vector
-	moveSpeed            float64
-	epsilon              float64
-
-	window  *glfw.Window
-	program uint32
-
-	KeyDowns map[string]bool
+// It creates a new camera with the necessary setup
+func CreateCamera() *camera.Camera {
+	camera := camera.NewCamera(mgl32.Vec3{-3, -5, 18.0}, mgl32.Vec3{0, 1, 0}, -90.0, 0.0)
+	camera.SetupProjection(45, float32(windowWidth)/float32(windowHeight), 0.1, 100.0)
+	return camera
 }
 
-func NewApplication() *Application {
-	var app Application
-	app.GenerateCube()
-	app.GenerateSphere()
-	app.moveSpeed = 1.0 / 1000.0
-	app.epsilon = 50.0
-	app.camera = camera.NewCamera(mgl32.Vec3{-3, -5, 18.0}, mgl32.Vec3{0, 1, 0}, -90.0, 0.0)
-	app.cameraDirection = 0.1
-	app.cameraDirectionSpeed = 5
-	fmt.Println("Camera state after new function")
-	fmt.Println(app.camera.Log())
-	// Rotation related code comes here.
-	app.camera.SetupProjection(45, float32(windowWidth)/float32(windowHeight), 0.1, 100.0)
-	fmt.Println("Camera state after setupProjection function")
-	fmt.Println(app.camera.Log())
-	app.cameraLastUpdate = time.Now().UnixNano()
-	app.KeyDowns = make(map[string]bool)
-	app.KeyDowns["W"] = false
-	app.KeyDowns["A"] = false
-	app.KeyDowns["S"] = false
-	app.KeyDowns["D"] = false
-	app.KeyDowns["Q"] = false
-	app.KeyDowns["E"] = false
-	app.KeyDowns["dLeft"] = false
-	app.KeyDowns["dRight"] = false
-	app.KeyDowns["dUp"] = false
-	app.KeyDowns["dDown"] = false
+// Create the keymap
+func SetupKeyMap() map[glfw.Key]bool {
+	keyDowns := make(map[glfw.Key]bool)
+	keyDowns[FORWARD] = false
+	keyDowns[LEFT] = false
+	keyDowns[RIGHT] = false
+	keyDowns[BACKWARD] = false
+	keyDowns[UP] = false
+	keyDowns[DOWN] = false
 
-	return &app
+	return keyDowns
+}
+
+// Update the z coordinates of the vectors.
+func Update() {
+	nowNano := time.Now().UnixNano()
+	delta := float64(nowNano - lastUpdate)
+	moveTime := delta / float64(time.Millisecond)
+	app.Update(delta)
+	lastUpdate = nowNano
+
+	forward := 0.0
+	if app.GetKeyState(FORWARD) && !app.GetKeyState(BACKWARD) {
+		forward = moveSpeed * moveTime
+	} else if app.GetKeyState(BACKWARD) && !app.GetKeyState(FORWARD) {
+		forward = -moveSpeed * moveTime
+	}
+	if forward != 0 {
+		app.GetCamera().Walk(float32(forward))
+	}
+	horisontal := 0.0
+	if app.GetKeyState(LEFT) && !app.GetKeyState(RIGHT) {
+		horisontal = -moveSpeed * moveTime
+	} else if app.GetKeyState(RIGHT) && !app.GetKeyState(LEFT) {
+		horisontal = moveSpeed * moveTime
+	}
+	if horisontal != 0 {
+		app.GetCamera().Strafe(float32(horisontal))
+	}
+	vertical := 0.0
+	if app.GetKeyState(UP) && !app.GetKeyState(DOWN) {
+		vertical = -moveSpeed * moveTime
+	} else if app.GetKeyState(DOWN) && !app.GetKeyState(UP) {
+		vertical = moveSpeed * moveTime
+	}
+	if vertical != 0 {
+		app.GetCamera().Lift(float32(vertical))
+	}
+
+	currX, currY := app.GetWindow().GetCursorPos()
+	x, y := trans.MouseCoordinates(currX, currY, windowWidth, windowHeight)
+	KeyDowns := make(map[string]bool)
+	// dUp
+	if y > 1.0-cameraDistance && y < 1.0 {
+		KeyDowns["dUp"] = true
+	} else {
+		KeyDowns["dUp"] = false
+	}
+	// dDown
+	if y < -1.0+cameraDistance && y > -1.0 {
+		KeyDowns["dDown"] = true
+	} else {
+		KeyDowns["dDown"] = false
+	}
+	// dLeft
+	if x < -1.0+cameraDistance && x > -1.0 {
+		KeyDowns["dLeft"] = true
+	} else {
+		KeyDowns["dLeft"] = false
+	}
+	// dRight
+	if x > 1.0-cameraDistance && x < 1.0 {
+		KeyDowns["dRight"] = true
+	} else {
+		KeyDowns["dRight"] = false
+	}
+
+	dX := float32(0.0)
+	dY := float32(0.0)
+	if KeyDowns["dUp"] && !KeyDowns["dDown"] {
+		dY = cameraDirectionSpeed
+	} else if KeyDowns["dDown"] && !KeyDowns["dUp"] {
+		dY = -cameraDirectionSpeed
+	}
+	if KeyDowns["dLeft"] && !KeyDowns["dRight"] {
+		dX = -cameraDirectionSpeed
+	} else if KeyDowns["dRight"] && !KeyDowns["dLeft"] {
+		dX = cameraDirectionSpeed
+	}
+	app.GetCamera().UpdateDirection(dX, dY)
 }
 
 // It generates a cube.
-func (a *Application) GenerateCube() {
-	a.cube = cube.NewCubeByVectorAndLength(vec.Vector{-0.5, -0.5, -0.5}, 1.0)
-	a.cubePosition = vec.Vector{0, 0, 1}
+func GenerateCube(shaderProgram *shader.Shader) {
+	colors := [6]mgl32.Vec3{
+		mgl32.Vec3{1.0, 0.0, 0.0},
+		mgl32.Vec3{1.0, 1.0, 0.0},
+		mgl32.Vec3{0.0, 1.0, 0.0},
+		mgl32.Vec3{0.0, 1.0, 1.0},
+		mgl32.Vec3{0.0, 0.0, 1.0},
+		mgl32.Vec3{1.0, 0.0, 1.0},
+	}
+	bottomCoordinates := [4]mgl32.Vec3{
+		mgl32.Vec3{-0.5, -0.5, -0.5},
+		mgl32.Vec3{-0.5, 2.0, 0.5},
+		mgl32.Vec3{2.0, 2.0, 0.5},
+		mgl32.Vec3{2.0, -0.5, -0.5},
+	}
+	bottomColor := [4]mgl32.Vec3{
+		colors[0],
+		colors[0],
+		colors[0],
+		colors[0],
+	}
+	bottomRect := rectangle.New(bottomCoordinates, bottomColor, shaderProgram)
+	cube := cuboid.New(bottomRect, 1.0, shaderProgram)
+	for i := 0; i < 6; i++ {
+		cube.SetSideColor(i, colors[i])
+	}
+	app.AddItem(cube)
 }
 
 // It generates a Sphere.
-func (a *Application) GenerateSphere() {
-	a.sphere = sp.NewSphere()
-	a.sphere.SetCenter(vec.Vector{0, 0, 5})
-	a.sphere.SetColor(vec.Vector{0, 0, 1})
-	a.sphere.SetRadius(2.0)
-}
-
-// Key handler function. it supports the debug option. (print out the points of the app)
-func (a *Application) KeyHandler() {
-	if a.window.GetKey(glfw.KeyH) == glfw.Press {
-		if !DebugPrint {
-			DebugPrint = true
-			fmt.Printf("app.camera: %s\n", a.camera.Log())
-			fmt.Printf("app.cube: %v\n", a.cube)
-			fmt.Printf("app.cubePosition: %v\n", a.cubePosition)
-		}
-	} else {
-		DebugPrint = false
-	}
-	if a.window.GetKey(glfw.KeyW) == glfw.Press {
-		a.KeyDowns["W"] = true
-	} else {
-		a.KeyDowns["W"] = false
-	}
-	if a.window.GetKey(glfw.KeyA) == glfw.Press {
-		a.KeyDowns["A"] = true
-	} else {
-		a.KeyDowns["A"] = false
-	}
-	if a.window.GetKey(glfw.KeyS) == glfw.Press {
-		a.KeyDowns["S"] = true
-	} else {
-		a.KeyDowns["S"] = false
-	}
-	if a.window.GetKey(glfw.KeyD) == glfw.Press {
-		a.KeyDowns["D"] = true
-	} else {
-		a.KeyDowns["D"] = false
-	}
-	if a.window.GetKey(glfw.KeyQ) == glfw.Press {
-		a.KeyDowns["Q"] = true
-	} else {
-		a.KeyDowns["Q"] = false
-	}
-	if a.window.GetKey(glfw.KeyE) == glfw.Press {
-		a.KeyDowns["E"] = true
-	} else {
-		a.KeyDowns["E"] = false
-	}
-	//calculate delta
-	nowUnix := time.Now().UnixNano()
-	delta := nowUnix - a.cameraLastUpdate
-	moveTime := float64(delta / int64(time.Millisecond))
-	// if the camera has been updated recently, we can skip now
-	if a.epsilon > moveTime {
-		return
-	}
-	a.cameraLastUpdate = nowUnix
-	// Move camera
-	forward := 0.0
-	if a.KeyDowns["W"] && !a.KeyDowns["S"] {
-		forward = a.moveSpeed * moveTime
-	} else if a.KeyDowns["S"] && !a.KeyDowns["W"] {
-		forward = -a.moveSpeed * moveTime
-	}
-	if forward != 0 {
-		a.camera.Walk(float32(forward))
-	}
-	horisontal := 0.0
-	if a.KeyDowns["A"] && !a.KeyDowns["D"] {
-		horisontal = -a.moveSpeed * moveTime
-	} else if a.KeyDowns["D"] && !a.KeyDowns["A"] {
-		horisontal = a.moveSpeed * moveTime
-	}
-	if horisontal != 0 {
-		a.camera.Strafe(float32(horisontal))
-	}
-	vertical := 0.0
-	if a.KeyDowns["Q"] && !a.KeyDowns["E"] {
-		vertical = -a.moveSpeed * moveTime
-	} else if a.KeyDowns["E"] && !a.KeyDowns["Q"] {
-		vertical = a.moveSpeed * moveTime
-	}
-	if vertical != 0 {
-		a.camera.Lift(float32(vertical))
-	}
-}
-
-/*
-* Mouse click handler logic:
-* - if the mouse moved - call the function with the delta values.
- */
-func (a *Application) MouseHandler() {
-	currX, currY := a.window.GetCursorPos()
-	x, y := trans.MouseCoordinates(currX, currY, windowWidth, windowHeight)
-	// dUp
-	if y > 1.0-a.cameraDirection && y < 1.0 {
-		a.KeyDowns["dUp"] = true
-	} else {
-		a.KeyDowns["dUp"] = false
-	}
-	// dDown
-	if y < -1.0+a.cameraDirection && y > -1.0 {
-		a.KeyDowns["dDown"] = true
-	} else {
-		a.KeyDowns["dDown"] = false
-	}
-	// dLeft
-	if x < -1.0+a.cameraDirection && x > -1.0 {
-		a.KeyDowns["dLeft"] = true
-	} else {
-		a.KeyDowns["dLeft"] = false
-	}
-	// dRight
-	if x > 1.0-a.cameraDirection && x < 1.0 {
-		a.KeyDowns["dRight"] = true
-	} else {
-		a.KeyDowns["dRight"] = false
-	}
-
-	dX := 0.0
-	dY := 0.0
-	if a.KeyDowns["dUp"] && !a.KeyDowns["dDown"] {
-		dY = 0.01 * a.cameraDirectionSpeed
-	} else if a.KeyDowns["dDown"] && !a.KeyDowns["dUp"] {
-		dY = -0.01 * a.cameraDirectionSpeed
-	}
-	if a.KeyDowns["dLeft"] && !a.KeyDowns["dRight"] {
-		dX = -0.01 * a.cameraDirectionSpeed
-	} else if a.KeyDowns["dRight"] && !a.KeyDowns["dLeft"] {
-		dX = 0.01 * a.cameraDirectionSpeed
-	}
-	a.camera.UpdateDirection(float32(dX), float32(dY))
-}
-
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(fmt.Errorf("could not initialize glfw: %v", err))
-	}
-
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.Resizable, glfw.True)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(windowWidth, windowHeight, windowTitle, nil, nil)
-
-	if err != nil {
-		panic(fmt.Errorf("could not create opengl renderer: %v", err))
-	}
-
-	window.MakeContextCurrent()
-
-	return window
-}
-
-func initOpenGL() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Println("OpenGL version", version)
-
-	vertexShader, err := shader.CompileShader(shader.VertexShaderModelViewProjectionSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-	fragmentShader, err := shader.CompileShader(shader.FragmentShaderBasicSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	program := gl.CreateProgram()
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.LinkProgram(program)
-	gl.ClearColor(0.3, 0.3, 0.3, 1.0)
-	gl.Viewport(0, 0, windowWidth, windowHeight)
-	return program
+func GenerateSphere(shaderProgram *shader.Shader) {
+	s := sphere.New(mgl32.Vec3{3, 3, 5}, mgl32.Vec3{0, 0, 1}, 2.0, shaderProgram)
+	s.SetPrecision(20)
+	app.AddItem(s)
 }
 
 func main() {
 	runtime.LockOSThread()
 
-	app := NewApplication()
-
-	app.window = initGlfw()
+	app = application.New()
+	app.SetWindow(application.InitGlfw(windowWidth, windowHeight, windowTitle))
 	defer glfw.Terminate()
-	app.program = initOpenGL()
+	application.InitOpenGL()
 
-	// Configure global settings
-	gl.UseProgram(app.program)
+	app.SetCamera(CreateCamera())
+
+	shaderProgram := shader.NewShader("examples/shapes-with-camera/vertexshader.vert", "examples/shapes-with-camera/fragmentshader.frag")
+	GenerateCube(shaderProgram)
+	GenerateSphere(shaderProgram)
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+	gl.ClearColor(0.3, 0.3, 0.3, 1.0)
+
+	lastUpdate = time.Now().UnixNano()
+	// register keyboard button callback
+	app.GetWindow().SetKeyCallback(app.KeyCallback)
 
 	gl.Enable(gl.DEPTH_TEST)
 	gl.DepthFunc(gl.LESS)
 
-	modelLocation := gl.GetUniformLocation(app.program, gl.Str("model\x00"))
-	// view aka camera
-	viewLocation := gl.GetUniformLocation(app.program, gl.Str("view\x00"))
-	projectionLocation := gl.GetUniformLocation(app.program, gl.Str("projection\x00"))
-	//CubePosition := app.cubePosition
-
-	for !app.window.ShouldClose() {
+	for !app.GetWindow().ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-		// mvp - modelview - projection matrix
-		V := app.camera.GetViewMatrix()
-		gl.UniformMatrix4fv(viewLocation, 1, false, &V[0])
-		// Should Be fine 'P'
-		P := app.camera.GetProjectionMatrix()
-		gl.UniformMatrix4fv(projectionLocation, 1, false, &P[0])
-		M := trans.TranslationMatrix(float32(app.cubePosition.X), float32(app.cubePosition.Y), float32(app.cubePosition.Z)).TransposeMatrix().GetMatrix()
-		gl.UniformMatrix4fv(modelLocation, 1, false, &M[0])
-		app.cube.Draw()
-		// Update M for the sphere
-		M = trans.TranslationMatrix(
-			float32(app.sphere.GetCenter().X),
-			float32(app.sphere.GetCenter().Y),
-			float32(app.sphere.GetCenter().Z)).Dot(trans.ScaleMatrix(
-			float32(app.sphere.GetRadius()),
-			float32(app.sphere.GetRadius()),
-			float32(app.sphere.GetRadius()))).TransposeMatrix().GetMatrix()
-		gl.UniformMatrix4fv(modelLocation, 1, false, &M[0])
-		app.sphere.Draw()
-		app.KeyHandler()
-		app.MouseHandler()
 		glfw.PollEvents()
-		app.window.SwapBuffers()
+		Update()
+		app.DrawWithUniforms()
+		app.GetWindow().SwapBuffers()
 	}
 }
