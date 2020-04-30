cd2f363dd6af72223638400ca6796dd2beb4f37c
akosgarai <szlem01@gmail.com>
2020-04-30 15:38:51 +0200

    Rectangle package - the square pkg is deprecated

---
+++ pkg/primitives/rectangle/rectangle_test.go
@@ -0,0 +1,247 @@
+package rectangle
+
+import (
+	"testing"
+
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+type testShader struct {
+}
+
+func (t testShader) Use() {
+}
+func (t testShader) SetUniformMat4(s string, m mgl32.Mat4) {
+}
+
+func TestNew(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+
+	if square.speed != 0.0 {
+		t.Error("Speed should be 0")
+	}
+	if square.direction.X() != 0.0 || square.direction.Y() != 0.0 || square.direction.Z() != 0.0 {
+		t.Error("Direction vector is not 0")
+	}
+
+	for i := 0; i < 4; i++ {
+		if square.points[i] != points[i] {
+			t.Error("Mismatch in the coordinates")
+		}
+		if square.colors[i] != colors[i] {
+			t.Error("Mismatch in the colors")
+		}
+	}
+}
+func TestSetColor(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	newColor := mgl32.Vec3{1, 1, 0}
+	square.SetColor(newColor)
+
+	for i := 0; i < 4; i++ {
+		if square.colors[i] != newColor {
+			t.Error("Mismatch in the colors")
+		}
+	}
+}
+func TestSetIndexColor(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	newColor := mgl32.Vec3{1, 1, 0}
+	square.SetIndexColor(0, newColor)
+
+	if square.colors[0] != newColor {
+		t.Error("Mismatch in the new color")
+	}
+	for i := 1; i < 4; i++ {
+		if square.colors[i] != colors[i] {
+			t.Error("Mismatch in the colors")
+		}
+	}
+}
+func TestLog(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	log := square.Log()
+	if len(log) < 10 {
+		t.Error("Log too short")
+	}
+}
+func TestSetupVao(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	if len(square.vao.Get()) != 0 {
+		t.Error("Vao is not empty before the first setup.")
+	}
+	square.setupVao()
+	if len(square.vao.Get()) == 0 {
+		t.Error("Vao is empty after the first setup.")
+	}
+}
+func TestBuildVao(t *testing.T) {
+	t.Skip("It needs opengl init.")
+}
+func TestDraw(t *testing.T) {
+	t.Skip("It needs opengl init.")
+}
+func TestDrawWithUniforms(t *testing.T) {
+	t.Skip("It needs opengl init.")
+}
+func TestUpdate(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	square.SetDirection(mgl32.Vec3{1, 0, 0})
+	square.SetSpeed(1)
+	dt := 10.0
+	square.Update(dt)
+	if square.points[0].X() != 10.0 || square.points[0].Y() != 0.0 || square.points[0].Z() != 0.0 {
+		t.Error("Invalid position for p0")
+	}
+	if square.points[1].X() != 11.0 || square.points[1].Y() != 0.0 || square.points[1].Z() != 0.0 {
+		t.Error("Invalid position for p1")
+	}
+	if square.points[2].X() != 11.0 || square.points[2].Y() != 1.0 || square.points[2].Z() != 0.0 {
+		t.Error("Invalid position for p2")
+	}
+	if square.points[3].X() != 10.0 || square.points[3].Y() != 1.0 || square.points[3].Z() != 0.0 {
+		t.Error("Invalid position for p3")
+	}
+}
+func TestSetDirection(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	newDirection := mgl32.Vec3{1, 1, 0}
+	square.SetDirection(newDirection)
+
+	if square.direction != newDirection {
+		t.Error("Mismatch in the direction")
+	}
+}
+func TestSetIndexDirection(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	square.SetIndexDirection(0, 1)
+
+	if square.direction.X() != 1.0 || square.direction.Y() != 0.0 || square.direction.Z() != 0.0 {
+		t.Error("Mismatch in the direction")
+	}
+}
+func TestSetSpeed(t *testing.T) {
+	points := [4]mgl32.Vec3{
+		mgl32.Vec3{0, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 1, 0},
+		mgl32.Vec3{0, 1, 0},
+	}
+	colors := [4]mgl32.Vec3{
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+		mgl32.Vec3{1, 0, 0},
+	}
+	var shader testShader
+	square := New(points, colors, shader)
+	square.SetSpeed(10)
+
+	if square.speed != 10.0 {
+		t.Error("Mismatch in the speed")
+	}
+}
