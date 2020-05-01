5a8c9e84f731bf54dc6e12ac6ab5e174046ed1b4
akosgarai <szlem01@gmail.com>
2020-05-01 21:15:11 +0200

    Update application

--- examples/06-draw-points-from-mouse-input/README.md
+++ examples/06-draw-points-from-mouse-input/README.md
@@ -2,36 +2,3 @@
 
 This application demonstrates the mouse handler options. The middle mouse button selects a new point. Every selected point is displayed on the screen.
 
-## Application
-
-This stucture represents our world. It contains the selected points. Every point contains the coordinate ([-1,1]) and the color ([0-1]). The AddPoint function is calle from the mouse event handler.
-
-### NewApplication
-
-It returns an application with the minimal setup.
-
-### MouseHandler
-
-This logic is responsible for handling the mouse events. It maintains 3 global variable. The `mousePositionX` is for storing the x coordinate of the mouse click, `mousePositionY` is for storing the y coordinate of the mouse click, `mouseButtonPressed` is for handling the button state. If the button is released, then the selected point (coordinates of the click event) is converted then inserted to the app.Points.
-
-### KeyHandler
-
-This is a basic function for supporting the debug. In case of the `D` button is clicked, it prints out the app.Points.
-
-### buildVAO
-
-This function builds the `[]float32`, that can be used as vertex data object. Currently it only inserts the coordinates to the vao.
-
-### Draw
-
-This function is responsible for drawing the points to the screen. It creates the vao, sets the buffer data, enables and sets the attribute arrays and then draw the points.
-
-## Functions
-
-- `initGlfw`
-
-Basic function for glfw initialization.
-
-- `initOpenGL`
-
-It is responsible for openGL initialization. It uses the `shader.FragmentShaderConstantSource` fragment shader and the `shader.VertexShaderPointSource` vertex shader.
--- examples/06-draw-points-from-mouse-input/app.go
+++ examples/06-draw-points-from-mouse-input/app.go
@@ -1,180 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"math/rand"
 	"runtime"
 
-	P "github.com/akosgarai/opengl_playground/pkg/primitives/point"
+	"github.com/akosgarai/opengl_playground/pkg/application"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/point"
 	trans "github.com/akosgarai/opengl_playground/pkg/primitives/transformations"
-	vec "github.com/akosgarai/opengl_playground/pkg/primitives/vector"
 	"github.com/akosgarai/opengl_playground/pkg/shader"
 
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
 )
 
 const (
 	windowWidth  = 800
 	windowHeight = 800
 	windowTitle  = "Example - draw points from mouse inputs"
+
+	LEFT_MOUSE_BUTTON = glfw.MouseButtonLeft
 )
 
 var (
-	DebugPrint         = false
-	mouseButtonPressed = false
-	mousePositionX     = 0.0
-	mousePositionY     = 0.0
+	addPoint = false
+
+	app    *application.Application
+	points *point.Points
 )
 
-type Application struct {
-	Points []P.Point
-
-	window  *glfw.Window
-	program uint32
-}
-
-func NewApplication() *Application {
-	return &Application{}
-}
-
-// AddPoint inserts a new point to the points.
-func (a *Application) AddPoint(point P.Point) {
-	a.Points = append(a.Points, point)
-}
-
-// Basic function for glfw initialization.
-func initGlfw() *glfw.Window {
-	if err := glfw.Init(); err != nil {
-		panic(fmt.Errorf("could not initialize glfw: %v", err))
+func Update() {
+	if !app.GetMouseButtonState(LEFT_MOUSE_BUTTON) && addPoint {
+		mX, mY := trans.MouseCoordinates(app.MousePosX, app.MousePosY, windowWidth, windowHeight)
+		coords := mgl32.Vec3{float32(mX), float32(mY), 0.0}
+		color := mgl32.Vec3{rand.Float32(), rand.Float32(), rand.Float32()}
+		size := float32(3 + rand.Intn(17))
+		points.Add(coords, color, size)
+		addPoint = false
+	} else if app.GetMouseButtonState(LEFT_MOUSE_BUTTON) {
+		addPoint = true
 	}
-
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.Resizable, glfw.True)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-	window, err := glfw.CreateWindow(windowWidth, windowHeight, windowTitle, nil, nil)
-
-	if err != nil {
-		panic(fmt.Errorf("could not create opengl renderer: %v", err))
-	}
-
-	window.MakeContextCurrent()
-
-	return window
-}
-
-func initOpenGL() uint32 {
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Println("OpenGL version", version)
-
-	vertexShader, err := shader.CompileShader(shader.VertexShaderPointSource, gl.VERTEX_SHADER)
-	if err != nil {
-		panic(err)
-	}
-	fragmentShader, err := shader.CompileShader(shader.FragmentShaderConstantSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		panic(err)
-	}
-
-	program := gl.CreateProgram()
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.LinkProgram(program)
-	return program
-}
-
-/*
-* Mouse click handler logic:
-* - if the left mouse button is not pressed, and the button is just released, App.AddPoint(), clean up the temp.point.
-* - if the button is just pressed, set the point that needs to be added.
- */
-func (a *Application) MouseHandler() {
-	x, y := a.window.GetCursorPos()
-
-	if a.window.GetMouseButton(glfw.MouseButtonMiddle) == glfw.Press {
-		if !mouseButtonPressed {
-			mousePositionX = x
-			mousePositionY = y
-			mouseButtonPressed = true
-		}
-	} else {
-		if mouseButtonPressed {
-			mouseButtonPressed = false
-			mX, mY := trans.MouseCoordinates(x, y, windowWidth, windowHeight)
-			a.AddPoint(
-				P.Point{
-					vec.Vector{mX, mY, 0.0},
-					vec.Vector{1, 1, 1},
-				})
-		}
-	}
-}
-
-// Key handler function. it supports the debug option. (print out the points of the app)
-func (a *Application) KeyHandler() {
-	if a.window.GetKey(glfw.KeyD) == glfw.Press {
-		if !DebugPrint {
-			DebugPrint = true
-			fmt.Println(a.Points)
-		}
-	} else {
-		DebugPrint = false
-	}
-}
-func (a *Application) buildVAO() []float32 {
-	var vao []float32
-	for _, item := range a.Points {
-		vao = append(vao, float32(item.Coordinate.X))
-		vao = append(vao, float32(item.Coordinate.Y))
-		vao = append(vao, float32(item.Coordinate.Z))
-	}
-	return vao
-}
-func (a *Application) Draw() {
-	if len(a.Points) < 1 {
-		return
-	}
-	points := a.buildVAO()
-	var vertexBufferObject uint32
-	gl.GenBuffers(1, &vertexBufferObject)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vertexBufferObject)
-	// a 32-bit float has 4 bytes, so we are saying the size of the buffer,
-	// in bytes, is 4 times the number of points
-	gl.BufferData(gl.ARRAY_BUFFER, 4*len(points), gl.Ptr(points), gl.STATIC_DRAW)
-
-	var vertexArrayObject uint32
-	gl.GenVertexArrays(1, &vertexArrayObject)
-	gl.BindVertexArray(vertexArrayObject)
-	// setup points
-	gl.EnableVertexAttribArray(0)
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 4*3, gl.PtrOffset(0))
-
-	gl.BindVertexArray(vertexArrayObject)
-	gl.DrawArrays(gl.POINTS, 0, int32(len(a.Points)))
 }
 
 func main() {
 	runtime.LockOSThread()
-	app := NewApplication()
-	app.window = initGlfw()
+	app = application.New()
+	app.SetWindow(application.InitGlfw(windowWidth, windowHeight, windowTitle))
 	defer glfw.Terminate()
-	app.program = initOpenGL()
+	application.InitOpenGL()
 
-	gl.UseProgram(app.program)
+	shaderProgram := shader.NewShader("examples/06-draw-points-from-mouse-input/vertexshader.vert", "examples/06-draw-points-from-mouse-input/fragmentshader.frag")
+	points = point.New(shaderProgram)
+	app.AddItem(points)
+	app.GetWindow().SetMouseButtonCallback(app.MouseButtonCallback)
 
 	gl.Enable(gl.PROGRAM_POINT_SIZE)
 
-	for !app.window.ShouldClose() {
+	for !app.GetWindow().ShouldClose() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
-		app.MouseHandler()
-		app.KeyHandler()
-		app.Draw()
 		glfw.PollEvents()
-		app.window.SwapBuffers()
+		Update()
+		if points.Count() > 0 {
+			app.Draw()
+		}
+		app.GetWindow().SwapBuffers()
 	}
 }
