011ab68622e80199192207994248db0c68a6bf69
akosgarai <szlem01@gmail.com>
2020-05-02 16:31:33 +0200

    Window pkg

---
+++ pkg/window/window.go
@@ -0,0 +1,30 @@
+package window
+
+import (
+	"fmt"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+// InitGlfw returns a *glfw.Windows instance.
+func InitGlfw(windowWidth, windowHeight int, windowTitle string) *glfw.Window {
+	if err := glfw.Init(); err != nil {
+		panic(fmt.Errorf("could not initialize glfw: %v", err))
+	}
+
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, windowTitle, nil, nil)
+
+	if err != nil {
+		panic(fmt.Errorf("could not create opengl renderer: %v", err))
+	}
+
+	window.MakeContextCurrent()
+
+	return window
+}
