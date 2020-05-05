3ed74a412ff6af9472e6e22ea3f9fae46bedd38c
akosgarai <szlem01@gmail.com>
2020-05-05 16:26:48 +0200

    Implement light package (#29)

---
+++ pkg/primitives/light/light.go
@@ -0,0 +1,54 @@
+package light
+
+import (
+	"github.com/go-gl/mathgl/mgl32"
+
+	trans "github.com/akosgarai/opengl_playground/pkg/primitives/transformations"
+)
+
+type Light struct {
+	position mgl32.Vec3
+
+	ambient  mgl32.Vec3
+	diffuse  mgl32.Vec3
+	specular mgl32.Vec3
+}
+
+func New(position, ambientComponent, diffuseComponent, specularComponent mgl32.Vec3) *Light {
+	return &Light{
+		position: position,
+		ambient:  ambientComponent,
+		diffuse:  diffuseComponent,
+		specular: specularComponent,
+	}
+}
+
+// Log returns the current state of the object
+func (l *Light) Log() string {
+	logString := "Light\n"
+	logString += " - Position: Vector{" + trans.Vec3ToString(l.position) + "}\n"
+	logString += " - Ambient: Vector{" + trans.Vec3ToString(l.ambient) + "}\n"
+	logString += " - Diffuse: Vector{" + trans.Vec3ToString(l.diffuse) + "}\n"
+	logString += " - Specualar: Vector{" + trans.Vec3ToString(l.specular) + "}\n"
+	return logString
+}
+
+// GetDiffuse returns the diffuse color of the material
+func (l *Light) GetAmbient() mgl32.Vec3 {
+	return l.ambient
+}
+
+// GetDiffuse returns the diffuse color of the material
+func (l *Light) GetDiffuse() mgl32.Vec3 {
+	return l.diffuse
+}
+
+// GetSpecular returns the specular color of the material
+func (l *Light) GetSpecular() mgl32.Vec3 {
+	return l.specular
+}
+
+// GetPosition returns the shininess of the material
+func (l *Light) GetPosition() mgl32.Vec3 {
+	return l.position
+}
