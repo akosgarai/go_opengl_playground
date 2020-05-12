08ba081b298775b503cf3591e132a9cd793b9529
akosgarai <szlem01@gmail.com>
2020-05-12 22:43:51 +0200

    Shader update vol 1
    eliminate functions file, NewShader function update

--- examples/model-loading/pkg/shader/shader.go
+++ examples/model-loading/pkg/shader/shader.go
@@ -1,11 +1,61 @@
 package shader
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	wrapper "github.com/akosgarai/opengl_playground/pkg/glwrapper"
+
+	"github.com/go-gl/mathgl/mgl32"
 )
+
+// LoadShaderFromFile takes a filepath string arguments.
+// It loads the file and returns it as a '\x00' terminated string.
+// It returns an error also.
+func LoadShaderFromFile(path string) (string, error) {
+	shaderCode, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	result := string(shaderCode) + "\x00"
+	return result, nil
+}
+
+// CompileShader creeates a shader, compiles the shader source, and returns
+// the uint32 identifier of the shader and nil. If the compile fails, it returns
+// an error and 0 as shader id.
+func CompileShader(source string, shaderType uint32) (uint32, error) {
+	shader := wrapper.CreateShader(shaderType)
+
+	csources, free := wrapper.Strs(source)
+	wrapper.ShaderSource(shader, 1, csources, nil)
+	free()
+	wrapper.CompileShader(shader)
+
+	var status int32
+	wrapper.GetShaderiv(shader, wrapper.COMPILE_STATUS, &status)
+	if status == wrapper.FALSE {
+		var logLength int32
+		wrapper.GetShaderiv(shader, wrapper.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		wrapper.GetShaderInfoLog(shader, logLength, nil, wrapper.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
 
 type Shader struct {
 	Id uint32
+
+	directionalLightSources []DirectionalLightSource
+	pointLightSources       []PointLightSource
+	spotLightSources        []SpotLightSource
+	viewPosition            mgl32.Vec3
+	viewPositionUniformName string
 }
 
 // NewShader returns a Shader. It's inputs are the filenames of the shaders.
@@ -34,7 +84,13 @@ func NewShader(vertexShaderPath, fragmentShaderPath string) *Shader {
 	wrapper.LinkProgram(program)
 
 	return &Shader{
-		Id: program,
+		Id:                      program,
+		directionalLightSources: []DirectionalLightSource{},
+		pointLightSources:       []PointLightSource{},
+		spotLightSources:        []SpotLightSource{},
+
+		viewPosition:            mgl32.Vec3{0, 0, 0},
+		viewPositionUniformName: "",
 	}
 }
 
