af69fad648f212bc61310acd629dd78952250a0b
akosgarai <szlem01@gmail.com>
2020-05-13 12:39:09 +0200

    Application improvement
    Some code from the shader moved here. Mostly the light handler part.

--- examples/model-loading/pkg/application/application.go
+++ examples/model-loading/pkg/application/application.go
@@ -15,6 +15,8 @@ type Shader interface {
 	Use()
 	SetUniformMat4(string, mgl32.Mat4)
 	GetId() uint32
+	SetUniform3f(string, float32, float32, float32)
+	SetUniform1f(string, float32)
 }
 
 type Camera interface {
@@ -34,6 +36,10 @@ type Application struct {
 	cameraSet bool
 
 	shaderMap map[Shader][]*mesh.Mesh
+
+	directionalLightSources []DirectionalLightSource
+	pointLightSources       []PointLightSource
+	spotLightSources        []SpotLightSource
 }
 
 type Window interface {
@@ -47,8 +53,11 @@ type Window interface {
 // New returns an application instance
 func New() *Application {
 	return &Application{
-		cameraSet: false,
-		shaderMap: make(map[Shader][]*mesh.Mesh),
+		cameraSet:               false,
+		shaderMap:               make(map[Shader][]*mesh.Mesh),
+		directionalLightSources: []DirectionalLightSource{},
+		pointLightSources:       []PointLightSource{},
+		spotLightSources:        []SpotLightSource{},
 	}
 }
 
@@ -87,19 +96,184 @@ func (a *Application) AddMeshToShader(m *mesh.Mesh, s Shader) {
 	a.shaderMap[s] = append(a.shaderMap[s], m)
 }
 
-// Draw calls Draw function in every drawable item.
+// Draw calls Draw function in every drawable item. It loops on the shaderMap (shaders).
+// For each shader, first set it to used state, setup camera realted uniforms,
+// then setup light related uniforms. Then we can pass the shader to the mesh for drawing.
 func (a *Application) Draw() {
 	for s, _ := range a.shaderMap {
 		s.Use()
 		if a.cameraSet {
 			s.SetUniformMat4("view", a.camera.GetViewMatrix())
 			s.SetUniformMat4("projection", a.camera.GetProjectionMatrix())
-			s.SetUniformMat4("viewPosition", a.camera.GetPosition())
+			cameraPos := a.camera.GetPosition()
+			s.SetUniform3f("viewPosition", cameraPos.X(), cameraPos.Y(), cameraPos.Z())
 		}
+		a.lightHandler(s)
 		for index, _ := range a.shaderMap[s] {
 			a.shaderMap[s][index].Draw(s)
 		}
 		// Set the lighting, viewPosition, etc uniforms.
 		// loop meshes, call the draw function.
 	}
+}
+
+// Setup light related uniforms.
+func (a *Application) lightHandler(s Shader) {
+	a.setupDirectionalLightForShader(s)
+	a.setupPointLightForShader(s)
+	a.setupSpotLightForShader(s)
+}
+
+// Setup directional light related uniforms. It iterates over the directional sources
+// and setups each uniform, where the name is not empty.
+func (a *Application) setupDirectionalLightForShader(s Shader) {
+	for _, source := range a.directionalLightSources {
+		if source.DirectionUniformName != "" {
+			direction := source.LightSource.GetDirection()
+			s.SetUniform3f(source.DirectionUniformName, direction.X(), direction.Y(), direction.Z())
+		}
+		if source.AmbientUniformName != "" {
+			ambient := source.LightSource.GetAmbient()
+			s.SetUniform3f(source.AmbientUniformName, ambient.X(), ambient.Y(), ambient.Z())
+		}
+		if source.DiffuseUniformName != "" {
+			diffuse := source.LightSource.GetDiffuse()
+			s.SetUniform3f(source.DiffuseUniformName, diffuse.X(), diffuse.Y(), diffuse.Z())
+		}
+		if source.SpecularUniformName != "" {
+			specular := source.LightSource.GetSpecular()
+			s.SetUniform3f(source.DiffuseUniformName, specular.X(), specular.Y(), specular.Z())
+		}
+	}
+
+}
+
+// Setup point light relates uniforms. It iterates over the point light sources and sets
+// up every uniform, where the name is not empty.
+func (a *Application) setupPointLightForShader(s Shader) {
+	for _, source := range a.pointLightSources {
+		if source.PositionUniformName != "" {
+			position := source.LightSource.GetPosition()
+			s.SetUniform3f(source.PositionUniformName, position.X(), position.Y(), position.Z())
+		}
+		if source.AmbientUniformName != "" {
+			ambient := source.LightSource.GetAmbient()
+			s.SetUniform3f(source.AmbientUniformName, ambient.X(), ambient.Y(), ambient.Z())
+		}
+		if source.DiffuseUniformName != "" {
+			diffuse := source.LightSource.GetDiffuse()
+			s.SetUniform3f(source.DiffuseUniformName, diffuse.X(), diffuse.Y(), diffuse.Z())
+		}
+		if source.SpecularUniformName != "" {
+			specular := source.LightSource.GetSpecular()
+			s.SetUniform3f(source.DiffuseUniformName, specular.X(), specular.Y(), specular.Z())
+		}
+		if source.ConstantTermUniformName != "" {
+			s.SetUniform1f(source.ConstantTermUniformName, source.LightSource.GetConstantTerm())
+		}
+		if source.LinearTermUniformName != "" {
+			s.SetUniform1f(source.LinearTermUniformName, source.LightSource.GetLinearTerm())
+		}
+		if source.QuadraticTermUniformName != "" {
+			s.SetUniform1f(source.QuadraticTermUniformName, source.LightSource.GetQuadraticTerm())
+		}
+	}
+}
+
+// Setup spot light related uniforms. It iterates over the spot light sources and sets up
+// every uniform, where the name is not empty.
+func (a *Application) setupSpotLightForShader(s Shader) {
+	for _, source := range a.spotLightSources {
+		if source.DirectionUniformName != "" {
+			direction := source.LightSource.GetDirection()
+			s.SetUniform3f(source.DirectionUniformName, direction.X(), direction.Y(), direction.Z())
+		}
+		if source.PositionUniformName != "" {
+			position := source.LightSource.GetPosition()
+			s.SetUniform3f(source.PositionUniformName, position.X(), position.Y(), position.Z())
+		}
+		if source.AmbientUniformName != "" {
+			ambient := source.LightSource.GetAmbient()
+			s.SetUniform3f(source.AmbientUniformName, ambient.X(), ambient.Y(), ambient.Z())
+		}
+		if source.DiffuseUniformName != "" {
+			diffuse := source.LightSource.GetDiffuse()
+			s.SetUniform3f(source.DiffuseUniformName, diffuse.X(), diffuse.Y(), diffuse.Z())
+		}
+		if source.SpecularUniformName != "" {
+			specular := source.LightSource.GetSpecular()
+			s.SetUniform3f(source.DiffuseUniformName, specular.X(), specular.Y(), specular.Z())
+		}
+		if source.ConstantTermUniformName != "" {
+			s.SetUniform1f(source.ConstantTermUniformName, source.LightSource.GetConstantTerm())
+		}
+		if source.LinearTermUniformName != "" {
+			s.SetUniform1f(source.LinearTermUniformName, source.LightSource.GetLinearTerm())
+		}
+		if source.QuadraticTermUniformName != "" {
+			s.SetUniform1f(source.QuadraticTermUniformName, source.LightSource.GetQuadraticTerm())
+		}
+		if source.CutoffUniformName != "" {
+			s.SetUniform1f(source.CutoffUniformName, source.LightSource.GetCutoff())
+		}
+		if source.OuterCutoffUniformName != "" {
+			s.SetUniform1f(source.OuterCutoffUniformName, source.LightSource.GetOuterCutoff())
+		}
+	}
+}
+
+// AddDirectionalLightSource sets up a directional light source.
+// It takes a DirectionalLight input that contains the model related info,
+// and it also takes a [4]string, with the uniform names that are used in the shader applications
+// the 'DirectionUniformName', 'AmbientUniformName', 'DiffuseUniformName', 'SpecularUniformName'.
+// They has to be in this order.
+func (a *Application) AddDirectionalLightSource(lightSource DirectionalLight, uniformNames [4]string) {
+	var dSource DirectionalLightSource
+	dSource.LightSource = lightSource
+	dSource.DirectionUniformName = uniformNames[0]
+	dSource.AmbientUniformName = uniformNames[1]
+	dSource.DiffuseUniformName = uniformNames[2]
+	dSource.SpecularUniformName = uniformNames[3]
+
+	a.directionalLightSources = append(a.directionalLightSources, dSource)
+}
+
+// AddPointLightSource sets up a point light source. It takes a PointLight
+// input that contains the model related info, and it also containt the uniform names in [7]string format.
+// The order has to be the following: 'PositionUniformName', 'AmbientUniformName', 'DiffuseUniformName',
+// 'SpecularUniformName', 'ConstantTermUniformName', 'LinearTermUniformName', 'QuadraticTermUniformName'.
+func (a *Application) AddPointLightSource(lightSource PointLight, uniformNames [7]string) {
+	var pSource PointLightSource
+	pSource.LightSource = lightSource
+	pSource.PositionUniformName = uniformNames[0]
+	pSource.AmbientUniformName = uniformNames[1]
+	pSource.DiffuseUniformName = uniformNames[2]
+	pSource.SpecularUniformName = uniformNames[3]
+	pSource.ConstantTermUniformName = uniformNames[4]
+	pSource.LinearTermUniformName = uniformNames[5]
+	pSource.QuadraticTermUniformName = uniformNames[6]
+
+	a.pointLightSources = append(a.pointLightSources, pSource)
+}
+
+// AddSpotLightSource sets up a spot light source. It takes a SpotLight input
+// that contains the model related info, and it also contains the uniform names in [10]string format.
+// The order has to be the following: 'PositionUniformName', 'DirectionUniformName', 'AmbientUniformName',
+// 'DiffuseUniformName', 'SpecularUniformName', 'ConstantTermUniformName', 'LinearTermUniformName',
+// 'QuadraticTermUniformName', 'CutoffUniformName'.
+func (a *Application) AddSpotLightSource(lightSource SpotLight, uniformNames [10]string) {
+	var sSource SpotLightSource
+	sSource.LightSource = lightSource
+	sSource.PositionUniformName = uniformNames[0]
+	sSource.DirectionUniformName = uniformNames[1]
+	sSource.AmbientUniformName = uniformNames[2]
+	sSource.DiffuseUniformName = uniformNames[3]
+	sSource.SpecularUniformName = uniformNames[4]
+	sSource.ConstantTermUniformName = uniformNames[5]
+	sSource.LinearTermUniformName = uniformNames[6]
+	sSource.QuadraticTermUniformName = uniformNames[7]
+	sSource.CutoffUniformName = uniformNames[8]
+	sSource.OuterCutoffUniformName = uniformNames[8]
+
+	a.spotLightSources = append(a.spotLightSources, sSource)
 }
