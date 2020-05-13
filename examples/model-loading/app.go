ab5cff8de8ed8093bf34bca868800509c4a1031f
akosgarai <szlem01@gmail.com>
2020-05-13 14:54:45 +0200

    application with mesh
    it seems that the textures needs to be double checked

---
+++ examples/model-loading/app.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/application"
+	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/mesh"
+	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/primitives"
+	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/shader"
+	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/texture"
+	wrapper "github.com/akosgarai/opengl_playground/pkg/glwrapper"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/camera"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/light"
+	"github.com/akosgarai/opengl_playground/pkg/window"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+const (
+	WindowWidth  = 800
+	WindowHeight = 800
+	WindowTitle  = "Example - mesh experiment"
+)
+
+var (
+	app *application.Application
+
+	Textures texture.Textures
+	Shader   *shader.Shader
+
+	DirectionalLightDirection = (mgl32.Vec3{0.7, 0.7, 0.7}).Normalize()
+	DirectionalLightAmbient   = mgl32.Vec3{0.1, 0.1, 0.1}
+	DirectionalLightDiffuse   = mgl32.Vec3{0.1, 0.1, 0.1}
+	DirectionalLightSpecular  = mgl32.Vec3{0.1, 0.1, 0.1}
+	PointLightAmbient         = mgl32.Vec3{0.5, 0.5, 0.5}
+	PointLightDiffuse         = mgl32.Vec3{0.5, 0.5, 0.5}
+	PointLightSpecular        = mgl32.Vec3{0.5, 0.5, 0.5}
+	PointLightPosition_1      = mgl32.Vec3{8, -0.5, -1.0}
+	LightConstantTerm         = float32(1.0)
+	LightLinearTerm           = float32(0.14)
+	LightQuadraticTerm        = float32(0.07)
+	SpotLightAmbient          = mgl32.Vec3{1, 1, 1}
+	SpotLightDiffuse          = mgl32.Vec3{1, 1, 1}
+	SpotLightSpecular         = mgl32.Vec3{1, 1, 1}
+	SpotLightDirection_1      = (mgl32.Vec3{0, 1, 0}).Normalize()
+	SpotLightPosition_1       = mgl32.Vec3{0.20, -6, -0.7}
+	SpotLightCutoff_1         = float32(4)
+	SpotLightOuterCutoff_1    = float32(5)
+)
+
+// It creates a new camera with the necessary setup
+func CreateCamera() *camera.Camera {
+	camera := camera.NewCamera(mgl32.Vec3{0.0, 10.0, 0.0}, mgl32.Vec3{0, 1, 0}, -90.0, -90.0)
+	camera.SetupProjection(45, float32(WindowWidth)/float32(WindowHeight), 0.1, 100.0)
+	return camera
+}
+
+func GenerateSquareMesh(t texture.Textures) *mesh.Mesh {
+	square := primitives.NewSquare()
+	v, i := square.MeshImput()
+	return mesh.New(v, i, t)
+}
+func main() {
+	runtime.LockOSThread()
+
+	app = application.New()
+	app.SetWindow(window.InitGlfw(WindowWidth, WindowHeight, WindowTitle))
+	defer glfw.Terminate()
+	wrapper.InitOpenGL()
+
+	app.SetCamera(CreateCamera())
+
+	Shader = shader.NewShader("examples/model-loading/shaders/texture.vert", "examples/model-loading/shaders/texture.frag")
+	app.AddShader(Shader)
+
+	Textures.AddTexture("examples/07-textured-lighting-map/colored-image-for-texture-testing-diffuse.png", wrapper.CLAMP_TO_EDGE, wrapper.CLAMP_TO_EDGE, wrapper.LINEAR, wrapper.LINEAR, "material.diffuse")
+	Textures.AddTexture("examples/07-textured-lighting-map/colored-image-for-texture-testing-specular.png", wrapper.CLAMP_TO_EDGE, wrapper.CLAMP_TO_EDGE, wrapper.LINEAR, wrapper.LINEAR, "material.specular")
+
+	squareMesh := GenerateSquareMesh(Textures)
+	app.AddMeshToShader(squareMesh, Shader)
+
+	// setup lighsources.
+	// directional light is coming from the up direction but not from too up.
+	DirectionalLightSource := light.NewDirectionalLight([4]mgl32.Vec3{
+		DirectionalLightDirection,
+		DirectionalLightAmbient,
+		DirectionalLightDiffuse,
+		DirectionalLightSpecular,
+	})
+	PointLightSource_1 := light.NewPointLight([4]mgl32.Vec3{
+		PointLightPosition_1,
+		PointLightAmbient,
+		PointLightDiffuse,
+		PointLightSpecular},
+		[3]float32{LightConstantTerm, LightLinearTerm, LightQuadraticTerm})
+	SpotLightSource_1 := light.NewSpotLight([5]mgl32.Vec3{
+		SpotLightPosition_1,
+		SpotLightDirection_1,
+		SpotLightAmbient,
+		SpotLightDiffuse,
+		SpotLightSpecular},
+		[5]float32{LightConstantTerm, LightLinearTerm, LightQuadraticTerm, SpotLightCutoff_1, SpotLightOuterCutoff_1})
+	app.AddDirectionalLightSource(DirectionalLightSource, [4]string{"dirLight[0].direction", "dirLight[0].ambient", "dirLight[0].diffuse", "dirLight[0].specular"})
+	app.AddPointLightSource(PointLightSource_1, [7]string{"pointLight[0].position", "pointLight[0].ambient", "pointLight[0].diffuse", "pointLight[0].specular", "pointLight[0].constant", "pointLight[0].linear", "pointLight[0].quadratic"})
+	app.AddSpotLightSource(SpotLightSource_1, [10]string{"spotLight[0].position", "spotLight[0].direction", "spotLight[0].ambient", "spotLight[0].diffuse", "spotLight[0].specular", "spotLight[0].constant", "spotLight[0].linear", "spotLight[0].quadratic", "spotLight[0].cutOff", "spotLight[0].outerCutOff"})
+
+	wrapper.Enable(wrapper.DEPTH_TEST)
+	wrapper.DepthFunc(wrapper.LESS)
+	wrapper.ClearColor(0.3, 0.3, 0.3, 1.0)
+
+	for !app.GetWindow().ShouldClose() {
+		wrapper.Clear(wrapper.COLOR_BUFFER_BIT | wrapper.DEPTH_BUFFER_BIT)
+		app.Draw()
+		glfw.PollEvents()
+		app.GetWindow().SwapBuffers()
+	}
+	fmt.Println(app.Log())
+}
