be95a4bad677be32df841d5e51f98a1143575a4f
akosgarai <szlem01@gmail.com>
2020-05-13 20:10:42 +0200

    Move camera from the application

--- examples/model-loading/app-light-movement.go
+++ examples/model-loading/app-light-movement.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"runtime"
+	"time"
 
 	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/application"
 	"github.com/akosgarai/opengl_playground/examples/model-loading/pkg/mesh"
@@ -11,6 +12,7 @@ import (
 	wrapper "github.com/akosgarai/opengl_playground/pkg/glwrapper"
 	"github.com/akosgarai/opengl_playground/pkg/primitives/camera"
 	"github.com/akosgarai/opengl_playground/pkg/primitives/light"
+	trans "github.com/akosgarai/opengl_playground/pkg/primitives/transformations"
 	"github.com/akosgarai/opengl_playground/pkg/window"
 
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -21,12 +23,26 @@ const (
 	WindowWidth  = 800
 	WindowHeight = 800
 	WindowTitle  = "Example - mesh experiment"
+
+	FORWARD  = glfw.KeyW
+	BACKWARD = glfw.KeyS
+	LEFT     = glfw.KeyA
+	RIGHT    = glfw.KeyD
+	UP       = glfw.KeyQ
+	DOWN     = glfw.KeyE
+
+	moveSpeed            = 0.005
+	cameraDirectionSpeed = float32(0.05)
+	CameraMoveSpeed      = 0.005
+	cameraDistance       = 0.1
 )
 
 var (
 	app *application.Application
 
 	Shader *shader.Shader
+
+	lastUpdate int64
 
 	DirectionalLightDirection = (mgl32.Vec3{0.7, 0.7, 0.7}).Normalize()
 	DirectionalLightAmbient   = mgl32.Vec3{0.5, 0.5, 0.5}
@@ -68,6 +84,80 @@ func GenerateCubeMesh(t texture.Textures, pos mgl32.Vec3) *mesh.Mesh {
 	m := mesh.New(v, i, t)
 	m.SetPosition(pos)
 	return m
+}
+func Update() {
+	nowNano := time.Now().UnixNano()
+	moveTime := float64(nowNano-lastUpdate) / float64(time.Millisecond)
+	lastUpdate = nowNano
+
+	forward := 0.0
+	if app.GetKeyState(FORWARD) && !app.GetKeyState(BACKWARD) {
+		forward = moveSpeed * moveTime
+	} else if app.GetKeyState(BACKWARD) && !app.GetKeyState(FORWARD) {
+		forward = -moveSpeed * moveTime
+	}
+	if forward != 0 {
+		app.GetCamera().Walk(float32(forward))
+	}
+	horizontal := 0.0
+	if app.GetKeyState(LEFT) && !app.GetKeyState(RIGHT) {
+		horizontal = -moveSpeed * moveTime
+	} else if app.GetKeyState(RIGHT) && !app.GetKeyState(LEFT) {
+		horizontal = moveSpeed * moveTime
+	}
+	if horizontal != 0 {
+		app.GetCamera().Strafe(float32(horizontal))
+	}
+	vertical := 0.0
+	if app.GetKeyState(UP) && !app.GetKeyState(DOWN) {
+		vertical = -moveSpeed * moveTime
+	} else if app.GetKeyState(DOWN) && !app.GetKeyState(UP) {
+		vertical = moveSpeed * moveTime
+	}
+	if vertical != 0 {
+		app.GetCamera().Lift(float32(vertical))
+	}
+	currX, currY := app.GetWindow().GetCursorPos()
+	x, y := trans.MouseCoordinates(currX, currY, WindowWidth, WindowHeight)
+	KeyDowns := make(map[string]bool)
+	// dUp
+	if y > 1.0-cameraDistance && y < 1.0 {
+		KeyDowns["dUp"] = true
+	} else {
+		KeyDowns["dUp"] = false
+	}
+	// dDown
+	if y < -1.0+cameraDistance && y > -1.0 {
+		KeyDowns["dDown"] = true
+	} else {
+		KeyDowns["dDown"] = false
+	}
+	// dLeft
+	if x < -1.0+cameraDistance && x > -1.0 {
+		KeyDowns["dLeft"] = true
+	} else {
+		KeyDowns["dLeft"] = false
+	}
+	// dRight
+	if x > 1.0-cameraDistance && x < 1.0 {
+		KeyDowns["dRight"] = true
+	} else {
+		KeyDowns["dRight"] = false
+	}
+
+	dX := float32(0.0)
+	dY := float32(0.0)
+	if KeyDowns["dUp"] && !KeyDowns["dDown"] {
+		dY = -cameraDirectionSpeed
+	} else if KeyDowns["dDown"] && !KeyDowns["dUp"] {
+		dY = cameraDirectionSpeed
+	}
+	if KeyDowns["dLeft"] && !KeyDowns["dRight"] {
+		dX = cameraDirectionSpeed
+	} else if KeyDowns["dRight"] && !KeyDowns["dLeft"] {
+		dX = -cameraDirectionSpeed
+	}
+	app.GetCamera().UpdateDirection(dX, dY)
 }
 func main() {
 	runtime.LockOSThread()
@@ -123,8 +213,12 @@ func main() {
 	wrapper.DepthFunc(wrapper.LESS)
 	wrapper.ClearColor(0.3, 0.3, 0.3, 1.0)
 
+	lastUpdate = time.Now().UnixNano()
+	app.GetWindow().SetKeyCallback(app.KeyCallback)
+
 	for !app.GetWindow().ShouldClose() {
 		wrapper.Clear(wrapper.COLOR_BUFFER_BIT | wrapper.DEPTH_BUFFER_BIT)
+		Update()
 		app.Draw()
 		glfw.PollEvents()
 		app.GetWindow().SwapBuffers()
