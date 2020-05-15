030bafa64ac27f56f2cfe8f2a8425bda925601c9
akosgarai <szlem01@gmail.com>
2020-05-15 10:27:00 +0200

    Add light realted interface definitions to interfaces pkg

--- pkg/interfaces/interfaces.go
+++ pkg/interfaces/interfaces.go
@@ -12,3 +12,30 @@ type Shader interface {
 	SetUniform1f(string, float32)
 	SetUniform1i(string, int32)
 }
+type DirectionalLight interface {
+	GetDirection() mgl32.Vec3
+	GetAmbient() mgl32.Vec3
+	GetDiffuse() mgl32.Vec3
+	GetSpecular() mgl32.Vec3
+}
+type PointLight interface {
+	GetPosition() mgl32.Vec3
+	GetAmbient() mgl32.Vec3
+	GetDiffuse() mgl32.Vec3
+	GetSpecular() mgl32.Vec3
+	GetConstantTerm() float32
+	GetLinearTerm() float32
+	GetQuadraticTerm() float32
+}
+type SpotLight interface {
+	GetPosition() mgl32.Vec3
+	GetDirection() mgl32.Vec3
+	GetAmbient() mgl32.Vec3
+	GetDiffuse() mgl32.Vec3
+	GetSpecular() mgl32.Vec3
+	GetConstantTerm() float32
+	GetLinearTerm() float32
+	GetQuadraticTerm() float32
+	GetCutoff() float32
+	GetOuterCutoff() float32
+}
