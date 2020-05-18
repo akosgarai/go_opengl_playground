89a854588d91c9898ac30005cd185d905c204617
akosgarai <szlem01@gmail.com>
2020-05-18 18:08:31 +0200

    Model loading implementation

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 
+	"github.com/akosgarai/opengl_playground/pkg/glwrapper"
 	"github.com/akosgarai/opengl_playground/pkg/interfaces"
+	"github.com/akosgarai/opengl_playground/pkg/mesh"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/material"
+	"github.com/akosgarai/opengl_playground/pkg/primitives/vertex"
 
-	"github.com/udhos/gwob"
+	"github.com/akosgarai/gwob"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+var (
+	glWrapper glwrapper.Wrapper
 )
 
 type Model struct {
@@ -23,8 +32,8 @@ func NewModelFromFile(path string) *Model {
 
 // Draw function loops over each of the meshes and calls ther Draw function.
 func (m *Model) Draw(s interfaces.Shader) {
-	for i, _ := range meshes {
-		meshes[i].Draw(s)
+	for i, _ := range m.meshes {
+		m.meshes[i].Draw(s)
 	}
 }
 func (m *Model) loadModel(path string) {
@@ -33,10 +42,51 @@ func (m *Model) loadModel(path string) {
 	o, errObj := gwob.NewObjFromFile(path, options)
 
 	if errObj != nil {
-		panic(err)
+		panic(errObj)
+	}
+	// load material lib
+	mtlLib, errMtl := gwob.ReadMaterialLibFromFile(o.Mtllib, options)
+	if errMtl != nil {
+		panic(errMtl)
 	}
 
 	for _, g := range o.Groups {
-		fmt.Println(g)
+		// Generate meshes. loop indexes from indexbegin, to indexbegin+indexcount.
+		var vertices vertex.Verticies
+		var indices []uint32
+		// It maps the gwob indexis to the current mesh indexes.
+		indexMap := make(map[int]uint32)
+		for i := g.IndexBegin; i < g.IndexBegin+g.IndexCount; i++ {
+			indexValue := o.Indices[i]
+			if _, ok := indexMap[indexValue]; !ok {
+				mappedValue := uint32(i)
+				indexMap[indexValue] = mappedValue
+				var vert vertex.Vertex
+				positionFirstIndex := indexValue * (o.StrideSize / 4)
+				vert.Position = mgl32.Vec3{o.Coord[positionFirstIndex], o.Coord[positionFirstIndex+1], o.Coord[positionFirstIndex+2]}
+				if o.TextCoordFound {
+					texIndex := positionFirstIndex + o.StrideOffsetTexture/4
+					vert.TexCoords = mgl32.Vec2{o.Coord[texIndex], o.Coord[texIndex+1]}
+				}
+				if o.NormCoordFound {
+					normIndex := positionFirstIndex + o.StrideOffsetNormal/4
+					vert.Normal = mgl32.Vec3{o.Coord[normIndex], o.Coord[normIndex+1], o.Coord[normIndex+2]}
+				}
+
+				vertices = append(vertices, vert)
+			}
+			indices = append(indices, indexMap[indexValue])
+		}
+		mtl, found := mtlLib.Lib[g.Usemtl]
+		if found {
+			mat := material.New(
+				mgl32.Vec3{mtl.Ka[0], mtl.Ka[1], mtl.Ka[2]},
+				mgl32.Vec3{mtl.Kd[0], mtl.Kd[1], mtl.Kd[2]},
+				mgl32.Vec3{mtl.Ks[0], mtl.Ks[1], mtl.Ks[2]},
+				float32(64),
+			)
+			materialMesh := mesh.NewMaterialMesh(vertices, indices, mat, glWrapper)
+			m.meshes = append(m.meshes, materialMesh)
+		}
 	}
 }
