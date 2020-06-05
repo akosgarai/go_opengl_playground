ddc4fe6e6a57f547a2fab94be31b8edee9d2ef77
akosgarai <szlem01@gmail.com>
2020-06-05 20:41:59 +0200

    Update 08 basic lightsource app - legacy camera movement handler

--- examples/08-basic-lightsource/app.go
+++ examples/08-basic-lightsource/app.go
@@ -25,14 +25,9 @@ const (
 	WindowHeight = 800
 	WindowTitle  = "Example - cubes, basic lighting"
 
-	FORWARD  = glfw.KeyW
-	BACKWARD = glfw.KeyS
-	LEFT     = glfw.KeyA
-	RIGHT    = glfw.KeyD
-	UP       = glfw.KeyQ
-	DOWN     = glfw.KeyE
-
-	moveSpeed = 0.005
+	CameraMoveSpeed      = 0.005
+	cameraDistance       = 0.1
+	cameraDirectionSpeed = float32(0.00500)
 )
 
 var (
@@ -40,17 +35,28 @@ var (
 
 	lastUpdate int64
 
-	cameraDistance       = 0.1
-	cameraDirectionSpeed = float32(0.00500)
-	Model                = model.New()
+	Model = model.New()
 
 	glWrapper wrapper.Wrapper
 )
+
+// Setup keymap for the camera movement
+func CameraMovementMap() map[string]glfw.Key {
+	cm := make(map[string]glfw.Key)
+	cm["forward"] = glfw.KeyW
+	cm["back"] = glfw.KeyS
+	cm["up"] = glfw.KeyQ
+	cm["down"] = glfw.KeyE
+	cm["left"] = glfw.KeyA
+	cm["right"] = glfw.KeyD
+	return cm
+}
 
 // It creates a new camera with the necessary setup
 func CreateCamera() *camera.Camera {
 	camera := camera.NewCamera(mgl32.Vec3{0, 0, 10.0}, mgl32.Vec3{0, 1, 0}, -90.0, 0.0)
 	camera.SetupProjection(45, float32(WindowWidth)/float32(WindowHeight), 0.1, 100.0)
+	camera.SetVelocity(CameraMoveSpeed)
 	return camera
 }
 
@@ -65,37 +71,10 @@ func CreateMaterialCubeMesh(position mgl32.Vec3, mat *material.Material) *mesh.M
 
 func Update() {
 	nowNano := time.Now().UnixNano()
-	delta := float64(nowNano - lastUpdate)
-	moveTime := delta / float64(time.Millisecond)
+	delta := float64(nowNano-lastUpdate) / float64(time.Millisecond)
 	lastUpdate = nowNano
+	app.Update(delta)
 
-	forward := 0.0
-	if app.GetKeyState(FORWARD) && !app.GetKeyState(BACKWARD) {
-		forward = moveSpeed * moveTime
-	} else if app.GetKeyState(BACKWARD) && !app.GetKeyState(FORWARD) {
-		forward = -moveSpeed * moveTime
-	}
-	if forward != 0 {
-		app.GetCamera().Walk(float32(forward))
-	}
-	horisontal := 0.0
-	if app.GetKeyState(LEFT) && !app.GetKeyState(RIGHT) {
-		horisontal = -moveSpeed * moveTime
-	} else if app.GetKeyState(RIGHT) && !app.GetKeyState(LEFT) {
-		horisontal = moveSpeed * moveTime
-	}
-	if horisontal != 0 {
-		app.GetCamera().Strafe(float32(horisontal))
-	}
-	vertical := 0.0
-	if app.GetKeyState(UP) && !app.GetKeyState(DOWN) {
-		vertical = -moveSpeed * moveTime
-	} else if app.GetKeyState(DOWN) && !app.GetKeyState(UP) {
-		vertical = moveSpeed * moveTime
-	}
-	if vertical != 0 {
-		app.GetCamera().Lift(float32(vertical))
-	}
 	currX, currY := app.GetWindow().GetCursorPos()
 	x, y := trans.MouseCoordinates(currX, currY, WindowWidth, WindowHeight)
 	KeyDowns := make(map[string]bool)
@@ -147,6 +126,7 @@ func main() {
 	glWrapper.InitOpenGL()
 
 	app.SetCamera(CreateCamera())
+	app.SetCameraMovementMap(CameraMovementMap())
 
 	lightSource := light.NewPointLight([4]mgl32.Vec3{mgl32.Vec3{-3, 0, -3}, mgl32.Vec3{1, 1, 1}, mgl32.Vec3{1, 1, 1}, mgl32.Vec3{1, 1, 1}}, [3]float32{1.0, 1.0, 1.0})
 	app.AddPointLightSource(lightSource, [7]string{"light.position", "light.ambient", "light.diffuse", "light.specular", "", "", ""})
