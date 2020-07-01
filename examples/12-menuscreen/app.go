c1d6598f2a1d7013e6de2d6002bde40fe3d8f7d0
akosgarai <szlem01@gmail.com>
2020-07-01 17:37:11 +0200

    Paper like texture to the background, attach text to it

--- examples/12-menuscreen/app.go
+++ examples/12-menuscreen/app.go
@@ -31,7 +31,7 @@ const (
 	WindowWidth  = 800
 	WindowHeight = 800
 	WindowTitle  = "Example - menu screen"
-	FontFile     = "/assets/fonts/Desyrel/desyrel.ttf"
+	FontFile     = "/assets/fonts/Desyrel/desyrel.regular.ttf"
 
 	CameraMoveSpeed      = 0.005
 	CameraDirectionSpeed = float32(0.050)
@@ -41,7 +41,7 @@ const (
 var (
 	app       *application.Application
 	glWrapper glwrapper.Wrapper
-	Textures  texture.Texture
+	PaperMesh *mesh.TexturedMesh
 
 	lastUpdate int64
 )
@@ -61,6 +61,14 @@ type Charset struct {
 	maxWidth, maxHeight int
 }
 
+func Paper(width, height float32) {
+	rect := rectangle.NewExact(width, height)
+	v, i, _ := rect.MeshInput()
+	var tex texture.Textures
+	tex.AddTexture(baseDir()+"/assets/paper.jpg", glwrapper.CLAMP_TO_EDGE, glwrapper.CLAMP_TO_EDGE, glwrapper.LINEAR, glwrapper.LINEAR, "paper", glWrapper)
+
+	PaperMesh = mesh.NewTexturedMesh(v, i, tex, glWrapper)
+}
 func (c *Charset) Print(text string, x, y, scale float32) {
 	indices := []rune(text)
 	fmt.Printf("The following text will be printed: '%s' as '%v'\n", text, indices)
@@ -79,21 +87,21 @@ func (c *Charset) Print(text string, x, y, scale float32) {
 		ch := c.fonts[runeIndex]
 		//calculate position and size for current rune
 		xpos := x + float32(ch.bearingWidth)*scale
-		ypos := y - float32(ch.glyphHeight-ch.bearingHeight)*scale
-		w := float32(ch.bearingWidth) * scale
-		h := float32(ch.bearingHeight) * scale
-		rect := rectangle.New(float32(ch.glyphWidth), float32(ch.glyphHeight))
+		ypos := y + float32(ch.glyphHeight-ch.bearingHeight)*scale
+		w := float32(ch.glyphWidth) * scale
+		h := float32(ch.glyphHeight) * scale
+		rect := rectangle.NewExact(w, h)
 		cols := []mgl32.Vec3{
 			mgl32.Vec3{0.0, 1.0, 0.0},
 		}
 		v, i, _ := rect.TexturedColoredMeshInput(cols)
-		position := mgl32.Vec3{xpos + w/2, 0, ypos + h/2}
+		position := mgl32.Vec3{x + float32(ch.bearingWidth+ch.glyphWidth/2)*scale, y - float32(ch.bearingHeight-ch.glyphHeight/2)*scale, -0.1}
 		msh := mesh.NewTexturedColoredMesh(v, i, ch.tex, cols, glWrapper)
-		msh.SetScale(mgl32.Vec3{scale, 1, scale})
-		msh.SetPosition(position.Mul(scale))
+		msh.SetPosition(position)
+		msh.SetParent(PaperMesh)
 		c.Model.AddMesh(msh)
-		fmt.Printf("pos: %#v\nch: %#v\nw: %f, h: %f, xpos: %f, ypos: %f, adv: %f\n\n", position.Mul(scale), ch, w, h, xpos, ypos, float32((ch.advance>>6))*scale)
-		x += float32((ch.advance >> 6)) * scale
+		fmt.Printf("%c %d\n\tpos: %#v\n\tch: %#v\n\tw: %f, h: %f, xpos: %f, ypos: %f, adv: %f\n", runeIndex, runeIndex, position, ch, w, h, xpos, ypos, float32(ch.advance)*scale)
+		x += float32(ch.advance) * scale
 	}
 }
 
@@ -177,7 +185,7 @@ func LoadCharset(fontFile string, low, high rune, scale float64) *Charset {
 			glyphHeight:   int(gh),
 			bearingWidth:  (int(gBnd.Min.X) >> 6),
 			bearingHeight: gdescent,
-			advance:       int(gAdv),
+			advance:       (int(gAdv) >> 6),
 			tex:           NewTextures(len(fonts), "tex", rgba),
 		}
 		if int(gw) > maxWidth {
@@ -186,11 +194,9 @@ func LoadCharset(fontFile string, low, high rune, scale float64) *Charset {
 		if int(gh) > maxHeight {
 			maxHeight = int(gh)
 		}
-		if ch == 119 {
-			fmt.Printf("Load:ch(119)g: %#v", g)
-		}
 		fonts[ch] = g
 	}
+	fmt.Printf("MaxWidth: %d, MaxHeight: %d\n", maxWidth, maxHeight)
 	return &Charset{model.New(), fonts, maxWidth, maxHeight}
 }
 func NewTextures(textureId int, uniformName string, rgba *image.RGBA) texture.Textures {
@@ -228,23 +234,31 @@ func main() {
 	defer glfw.Terminate()
 	glWrapper.InitOpenGL()
 
-	textureShader := shader.NewShader(baseDir()+"/shaders/font.vert", baseDir()+"/shaders/font.frag", glWrapper)
-	app.AddShader(textureShader)
+	fontShader := shader.NewShader(baseDir()+"/shaders/font.vert", baseDir()+"/shaders/font.frag", glWrapper)
+	app.AddShader(fontShader)
+	paperShader := shader.NewShader(baseDir()+"/shaders/paper.vert", baseDir()+"/shaders/paper.frag", glWrapper)
+	app.AddShader(paperShader)
 
 	glWrapper.Enable(glwrapper.DEPTH_TEST)
 	glWrapper.DepthFunc(glwrapper.LESS)
 	glWrapper.Enable(glwrapper.BLEND)
 	glWrapper.BlendFunc(glwrapper.SRC_APLHA, glwrapper.ONE_MINUS_SRC_ALPHA)
 	glWrapper.ClearColor(0.0, 0.25, 0.5, 1.0)
+	paperModel := model.New()
+	Paper(1, 1)
+	PaperMesh.RotateX(-90)
+	PaperMesh.SetPosition(mgl32.Vec3{-0.4, -0.3, -0.0})
+	paperModel.AddMesh(PaperMesh)
+
+	app.AddModelToShader(paperModel, paperShader)
 
 	lastUpdate = time.Now().UnixNano()
 	// register keyboard button callback
 	app.GetWindow().SetKeyCallback(app.KeyCallback)
 	Fonts := LoadCharset(FontFile, 32, 127, 40.0)
-	Fonts.Print("How are You?", 0, 0, 0.05)
-	Fonts.RotateX(-90)
+	Fonts.Print("How are You?", -0.5, 0.2, 3.0/float32(WindowWidth))
 	Fonts.SetTransparent(true)
-	app.AddModelToShader(Fonts, textureShader)
+	app.AddModelToShader(Fonts, fontShader)
 
 	for !app.GetWindow().ShouldClose() {
 		glWrapper.Clear(glwrapper.COLOR_BUFFER_BIT | glwrapper.DEPTH_BUFFER_BIT)
