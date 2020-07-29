4524a9a594ae0b950397d647e0562ce5e034f718
akosgarai <szlem01@gmail.com>
2020-07-29 18:00:48 +0200

    Options to 02 static triangle app

--- examples/02-static-triangle/app.go
+++ examples/02-static-triangle/app.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"os"
 	"path"
 	"runtime"
+	"time"
 
 	"github.com/akosgarai/playground_engine/pkg/application"
+	"github.com/akosgarai/playground_engine/pkg/config"
 	"github.com/akosgarai/playground_engine/pkg/glwrapper"
 	"github.com/akosgarai/playground_engine/pkg/interfaces"
+	"github.com/akosgarai/playground_engine/pkg/material"
 	"github.com/akosgarai/playground_engine/pkg/mesh"
 	"github.com/akosgarai/playground_engine/pkg/model"
 	"github.com/akosgarai/playground_engine/pkg/primitives/triangle"
 	"github.com/akosgarai/playground_engine/pkg/screen"
 	"github.com/akosgarai/playground_engine/pkg/shader"
+	"github.com/akosgarai/playground_engine/pkg/texture"
 	"github.com/akosgarai/playground_engine/pkg/window"
 
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -19,21 +24,38 @@ import (
 )
 
 const (
-	WindowWidth  = 800
-	WindowHeight = 800
-	WindowTitle  = "Example - static triangle"
+	WindowWidth   = 800
+	WindowHeight  = 800
+	WindowTitle   = "Example - static triangle"
+	FORM_ENV_NAME = "SETTINGS"
+	ON_VALUE      = "on"
+	FontFile      = "/../../assets/fonts/Desyrel/desyrel.regular.ttf"
 )
 
 var (
 	color = []mgl32.Vec3{mgl32.Vec3{0, 1, 0}}
 
-	app *application.Application
+	app            *application.Application
+	SettingsScreen *screen.FormScreen
+	MenuScreen     *screen.MenuScreen
+	AppScreen      *screen.Screen
+	Settings       = config.New()
 
 	glWrapper glwrapper.Wrapper
+
+	DefaultMaterial   = material.Jade
+	HighlightMaterial = material.Ruby
+	lastUpdate        int64
+	startTime         int64
 )
 
-func GenerateColoredMesh(col []mgl32.Vec3) *mesh.ColorMesh {
+func InitSettings() {
+	Settings.AddConfig("ClearCol", "BG color", "The clear color of the window. It is used as the color of the background.", mgl32.Vec3{0.0, 0.0, 0.0}, nil)
+	Settings.AddConfig("ItemColor", "Item color", "The color of the item.", mgl32.Vec3{0.0, 1.0, 0.0}, nil)
+}
+func GenerateColoredMesh(color mgl32.Vec3) *mesh.ColorMesh {
 	triang := triangle.New(60, 60, 60)
+	col := []mgl32.Vec3{color}
 	v, i, _ := triang.ColoredMeshInput(col)
 	return mesh.NewColorMesh(v, i, col, glWrapper)
 }
@@ -44,32 +66,144 @@ func baseDir() string {
 func setupApp(glWrapper interfaces.GLWrapper) {
 	glWrapper.Enable(glwrapper.DEPTH_TEST)
 	glWrapper.DepthFunc(glwrapper.LESS)
+	clearColor := Settings["ClearCol"].GetCurrentValue().(mgl32.Vec3)
+	glWrapper.ClearColor(clearColor.X(), clearColor.Y(), clearColor.Z(), 1.0)
 }
-
-func main() {
-	runtime.LockOSThread()
-
-	app = application.New(glWrapper)
-	app.SetWindow(window.InitGlfw(WindowWidth, WindowHeight, WindowTitle))
-	defer glfw.Terminate()
-	glWrapper.InitOpenGL()
-
+func AddFormScreen() bool {
+	val := os.Getenv(FORM_ENV_NAME)
+	if val == ON_VALUE {
+		return true
+	}
+	return false
+}
+func mainScreen() *screen.Screen {
 	scrn := screen.New()
 	shaderProgram := shader.NewShader(baseDir()+"/shaders/vertexshader.vert", baseDir()+"/shaders/fragmentshader.frag", glWrapper)
 	scrn.AddShader(shaderProgram)
 
-	triang := GenerateColoredMesh(color)
+	itemColor := Settings["ItemColor"].GetCurrentValue().(mgl32.Vec3)
+	triang := GenerateColoredMesh(itemColor)
 	mod := model.New()
 	mod.AddMesh(triang)
 	mod.RotateX(90)
 	scrn.AddModelToShader(mod, shaderProgram)
 	scrn.Setup(setupApp)
+	return scrn
+}
+func createMenu() *screen.MenuScreen {
+	var tex texture.Textures
+	tex.TransparentTexture(1, 1, 0, "paper", glWrapper)
+	builder := screen.NewMenuScreenBuilder()
+	builder.SetWrapper(glWrapper)
+	builder.SetWindowSize(float32(WindowWidth), float32(WindowHeight))
+	builder.SetFrameMaterial(DefaultMaterial)
+	builder.SetBackgroundColor(DefaultMaterial.GetAmbient())
+	builder.SetMenuItemSurfaceTexture(tex)
+	builder.SetMenuItemDefaultMaterial(DefaultMaterial)
+	builder.SetMenuItemHighlightMaterial(HighlightMaterial)
+	builder.SetMenuItemFontColor(mgl32.Vec3{0, 0, 0})
+
+	MenuFonts, err := model.LoadCharset(baseDir()+FontFile, 32, 127, 40.0, 72, glWrapper)
+	if err != nil {
+		panic(err)
+	}
+	MenuFonts.SetTransparent(true)
+	builder.SetCharset(MenuFonts)
+	contS := func(m map[string]bool) bool {
+		return m["world-started"]
+	}
+	contNS := func(m map[string]bool) bool {
+		return !m["world-started"]
+	}
+	contAll := func(m map[string]bool) bool {
+		return true
+	}
+	restartEvent := func() {
+		lastUpdate = time.Now().UnixNano()
+		startTime = lastUpdate
+		AppScreen = mainScreen()
+		app.ActivateScreen(AppScreen)
+	}
+	startEvent := func() {
+		lastUpdate = time.Now().UnixNano()
+		startTime = lastUpdate
+		AppScreen = mainScreen()
+		app.ActivateScreen(AppScreen)
+		MenuScreen.SetState("world-started", true)
+		MenuScreen.BuildScreen()
+	}
+	settingsEvent := func() {
+		app.ActivateScreen(SettingsScreen)
+	}
+	continueEvent := func() {
+		app.ActivateScreen(AppScreen)
+	}
+	exitEvent := func() {
+		app.GetWindow().SetShouldClose(true)
+	}
+	cont := screen.NewMenuScreenOption("continue", contS, continueEvent)
+	builder.AddOption(*cont) // continue
+	start := screen.NewMenuScreenOption("start", contNS, startEvent)
+	builder.AddOption(*start) // start
+	restart := screen.NewMenuScreenOption("restart", contS, restartEvent)
+	builder.AddOption(*restart) // restart
+	settings := screen.NewMenuScreenOption("settings", contAll, settingsEvent)
+	builder.AddOption(*settings) // settings
+	exit := screen.NewMenuScreenOption("exit", contAll, exitEvent)
+	builder.AddOption(*exit) // exit
+	return builder.Build()
+}
+func createSettings(defaults config.Config) *screen.FormScreen {
+	formItemOrders := []string{
+		"ClearCol",
+		"ItemColor",
+	}
+	builder := screen.NewFormScreenBuilder()
+	builder.SetWrapper(glWrapper)
+	builder.SetWindowSize(float32(WindowWidth), float32(WindowHeight))
+	builder.SetFrameMaterial(material.Ruby)
+	builder.SetHeaderLabel("Settings")
+	builder.SetConfig(defaults)
+	builder.SetConfigOrder(formItemOrders)
+	return builder.Build()
+}
+func Update() {
+	nowNano := time.Now().UnixNano()
+	delta := float64(nowNano-lastUpdate) / float64(time.Millisecond)
+	lastUpdate = nowNano
+	app.SetUniformFloat("time", float32(float64(nowNano-startTime)/float64(time.Second)))
+	app.Update(delta)
+}
+
+func main() {
+	runtime.LockOSThread()
+
+	InitSettings()
+	app = application.New(glWrapper)
+	app.SetWindow(window.InitGlfw(WindowWidth, WindowHeight, WindowTitle))
+	defer glfw.Terminate()
+	glWrapper.InitOpenGL()
+
+	scrn := mainScreen()
 	app.AddScreen(scrn)
-	app.ActivateScreen(scrn)
+	if AddFormScreen() {
+		app.GetWindow().SetKeyCallback(app.KeyCallback)
+		app.GetWindow().SetMouseButtonCallback(app.MouseButtonCallback)
+		app.GetWindow().SetCharCallback(app.CharCallback)
+		MenuScreen = createMenu()
+		app.AddScreen(MenuScreen)
+		app.MenuScreen(MenuScreen)
+		SettingsScreen = createSettings(Settings)
+		app.AddScreen(SettingsScreen)
+		app.ActivateScreen(SettingsScreen)
+	} else {
+		app.ActivateScreen(scrn)
+	}
 
 	for !app.GetWindow().ShouldClose() {
 		glWrapper.Clear(glwrapper.COLOR_BUFFER_BIT | glwrapper.DEPTH_BUFFER_BIT)
 		app.Draw(glWrapper)
+		Update()
 		glfw.PollEvents()
 		app.GetWindow().SwapBuffers()
 	}
