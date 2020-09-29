480a05649afbbf5b4560e03fa41813b62c389185
akosgarai <szlem01@gmail.com>
2020-09-29 14:36:03 +0200

    First draft of the frame app

---
+++ examples/02-static-frame/README.md
@@ -0,0 +1,7 @@
+# Static frame app
+
+The purpose of this app to draw a static frame to the screen. The frame has to be fine for every screen size.
+The first iteration the following features were implemented:
+- Env handling in the init function. the width and height of the window could be managed by setting the ´WIDTH´ and the ´HEIGHT´ envs.
+- Default window size: 800 * 800.
+- Shader applications for the sample app.
---
+++ examples/02-static-frame/app.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"os"
+	"path"
+	"runtime"
+	"strconv"
+
+	"github.com/akosgarai/playground_engine/pkg/application"
+	"github.com/akosgarai/playground_engine/pkg/glwrapper"
+	"github.com/akosgarai/playground_engine/pkg/interfaces"
+	"github.com/akosgarai/playground_engine/pkg/mesh"
+	"github.com/akosgarai/playground_engine/pkg/model"
+	"github.com/akosgarai/playground_engine/pkg/primitives/rectangle"
+	"github.com/akosgarai/playground_engine/pkg/screen"
+	"github.com/akosgarai/playground_engine/pkg/shader"
+	"github.com/akosgarai/playground_engine/pkg/window"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+const (
+	WindowTitle = "Example - static frame"
+)
+
+var (
+	app *application.Application
+
+	glWrapper glwrapper.Wrapper
+
+	lastUpdate int64
+	startTime  int64
+
+	WindowWidth  = 800
+	WindowHeight = 800
+)
+
+func init() {
+	width := os.Getenv("WIDTH")
+	if width != "" {
+		val, err := strconv.Atoi(width)
+		if err == nil {
+			WindowWidth = val
+		}
+	}
+	height := os.Getenv("HEIGHT")
+	if height != "" {
+		val, err := strconv.Atoi(height)
+		if err == nil {
+			WindowHeight = val
+		}
+	}
+}
+
+func baseDir() string {
+	_, filename, _, _ := runtime.Caller(1)
+	return path.Dir(filename)
+}
+
+func setupApp(glWrapper interfaces.GLWrapper) {
+	glWrapper.Enable(glwrapper.DEPTH_TEST)
+	glWrapper.DepthFunc(glwrapper.LESS)
+	glWrapper.ClearColor(1.0, 1.0, 0.0, 1.0)
+}
+
+func mainScreen() *screen.Screen {
+	scrn := screen.New()
+	scrn.Setup(setupApp)
+	// add shader program
+	shaderProgram := shader.NewShader(baseDir()+"/shaders/vertexshader.vert", baseDir()+"/shaders/fragmentshader.frag", glWrapper)
+	scrn.AddShader(shaderProgram)
+	// models: 4 rectangle.
+	// - top and bottom: 2 * 0.5, vertical positions: 0.75, -0.75, color: 0,0,1
+	// - left, right: 1 * 0.5 (2 - 2*widthofthetopbottom), horizontal positions: 0.75, -0.75, color: 0,1,1
+	mod := model.New()
+	squareBaseVertical := rectangle.NewExact(2.0, 0.5)
+	verticalFrameColor := []mgl32.Vec3{mgl32.Vec3{0, 0, 1}}
+	v, i, _ := squareBaseVertical.ColoredMeshInput(verticalFrameColor)
+	// top mesh
+	mshTop := mesh.NewColorMesh(v, i, verticalFrameColor, glWrapper)
+	mshTop.SetPosition(mgl32.Vec3{0, 0.75, 0})
+	mod.AddMesh(mshTop)
+	// bottom mesh
+	mshBottom := mesh.NewColorMesh(v, i, verticalFrameColor, glWrapper)
+	mshBottom.SetPosition(mgl32.Vec3{0, -0.75, 0})
+	mod.AddMesh(mshBottom)
+
+	squareBaseHorizontal := rectangle.NewExact(0.5, 1.0)
+	horizontalFrameColor := []mgl32.Vec3{mgl32.Vec3{0, 1, 1}}
+	v, i, _ = squareBaseHorizontal.ColoredMeshInput(horizontalFrameColor)
+	// left mesh
+	mshLeft := mesh.NewColorMesh(v, i, horizontalFrameColor, glWrapper)
+	mshLeft.SetPosition(mgl32.Vec3{-0.75, 0, 0})
+	mod.AddMesh(mshLeft)
+	// right mesh
+	mshRight := mesh.NewColorMesh(v, i, horizontalFrameColor, glWrapper)
+	mshRight.SetPosition(mgl32.Vec3{0.75, 0, 0})
+	mod.AddMesh(mshRight)
+	mod.RotateX(90)
+	scrn.AddModelToShader(mod, shaderProgram)
+	return scrn
+}
+
+func main() {
+	runtime.LockOSThread()
+
+	app = application.New(glWrapper)
+	app.SetWindow(window.InitGlfw(WindowWidth, WindowHeight, WindowTitle))
+	defer glfw.Terminate()
+	glWrapper.InitOpenGL()
+
+	scrn := mainScreen()
+	app.AddScreen(scrn)
+	app.ActivateScreen(scrn)
+
+	for !app.GetWindow().ShouldClose() {
+		glWrapper.Clear(glwrapper.COLOR_BUFFER_BIT | glwrapper.DEPTH_BUFFER_BIT)
+		app.Draw(glWrapper)
+		glfw.PollEvents()
+		app.GetWindow().SwapBuffers()
+	}
+}
