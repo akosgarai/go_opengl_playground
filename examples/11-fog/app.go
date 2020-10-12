f7b5009675dc0c974a06c39a2d0bc81c7d8eb322
akosgarai <szlem01@gmail.com>
2020-10-12 21:39:33 +0200

    Update 11 - fog app

--- examples/11-fog/app.go
+++ examples/11-fog/app.go
@@ -26,7 +26,7 @@ const (
 
 	CameraMoveSpeed      = 0.005
 	CameraDirectionSpeed = float32(0.050)
-	CameraDistance       = 0.1
+	CameraDistance       = float32(0.1)
 )
 
 var (
@@ -43,15 +43,17 @@ var (
 	DirectionalLightSpecular  = mgl32.Vec3{0.3, 0.3, 0.3}
 )
 
-// Setup keymap for the camera movement
-func CameraMovementMap() map[string]glfw.Key {
-	cm := make(map[string]glfw.Key)
-	cm["forward"] = glfw.KeyW
-	cm["back"] = glfw.KeyS
-	cm["up"] = glfw.KeyQ
-	cm["down"] = glfw.KeyE
-	cm["left"] = glfw.KeyA
-	cm["right"] = glfw.KeyD
+// Setup options for the camera
+func CameraMovementOptions() map[string]interface{} {
+	cm := make(map[string]interface{})
+	cm["forward"] = []glfw.Key{glfw.KeyW}
+	cm["back"] = []glfw.Key{glfw.KeyS}
+	cm["up"] = []glfw.Key{glfw.KeyQ}
+	cm["down"] = []glfw.Key{glfw.KeyE}
+	cm["left"] = []glfw.Key{glfw.KeyA}
+	cm["right"] = []glfw.Key{glfw.KeyD}
+	cm["rotateOnEdgeDistance"] = CameraDistance
+	cm["mode"] = "default"
 	return cm
 }
 func CreateGround() {
@@ -104,9 +106,7 @@ func main() {
 	glWrapper.InitOpenGL()
 
 	scrn := screen.New()
-	scrn.SetCamera(CreateCamera())
-	scrn.SetCameraMovementMap(CameraMovementMap())
-	scrn.SetRotateOnEdgeDistance(CameraDistance)
+	scrn.SetupCamera(CreateCamera(), CameraMovementOptions())
 
 	shaderProgramFog := shader.NewShader(baseDir()+"/shaders/fog.vert", baseDir()+"/shaders/fog.frag", glWrapper)
 	scrn.AddShader(shaderProgramFog)
